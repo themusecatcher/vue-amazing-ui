ed3c55077f9a85595d533ebb892b00a48bbe1e6d
themusecatcher <themusecatcher@163.com>
2023-02-21 10:25:23 +0800

    chore: update Breadcrumb

--- packages/index.ts
+++ packages/index.ts
@@ -1,15 +1,17 @@
-// 方便管理所有组件
+import type { App } from 'vue'
 import Pagination from './pagination'
 import Breadcrumb from './breadcrumb'
 
+// 所有组件列表
 const components = [
   Pagination,
   Breadcrumb
 ]
 
-// 提供全局 install 方法
-const install = app => {
-  components.forEach(comp => app.use(comp))
+// 定义 install 方法
+const install = (app: App): void => {
+  // 遍历注册所有组件
+  components.forEach(component => app.component(component.name, component))
 }
 
 const VueAmazingUi = {
--- packages/pagination/index.ts
+++ packages/pagination/index.ts
@@ -1,7 +1,8 @@
+import type { App } from 'vue'
 import Pagination from './Pagination.vue'
 
 // 使用install方法，在app.use挂载
-Pagination.install = app => {
+Pagination.install = (app: App): void => {
   app.component(Pagination.name, Pagination)
 }
 
