d0f864f9a541eeab3ad2c36befbfbbc8bce5b9da
themusecatcher <themusecatcher@163.com>
2023-02-23 14:48:28 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -49,14 +49,18 @@ export default defineConfig({
         当设置为 'terser' 时必须先安装 Terser。（yarn add terser -D）
     */
     minify: 'terser', // Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效
-    terserOptions: { // 在打包代码时移除 console.log、debugger 和 注释
-      compress: { 
-        drop_console: false,
-        drop_debugger: true,
-        pure_funcs: ['console.log']
+    terserOptions: { // 在打包代码时移除 console、debugger 和 注释
+      compress: {
+        /* (default: false) -- Pass true to discard calls to console.* functions.
+          If you wish to drop a specific function call such as console.info and/or
+          retain side effects from function arguments after dropping the function
+          call then use pure_funcs instead
+        */
+        drop_console: true, // 生产环境时移除console
+        drop_debugger: true
       },
       format: {
-        comments: false // 删除注释
+        comments: false // 删除注释comments
       }
     }
   }
