15d93663f32c27bdaefae808a8d1b99046916238
themusecatcher <themusecatcher@163.com>
2023-03-17 10:09:01 +0800

    chore: update Swiper

--- src/views/Swiper.vue
+++ src/views/Swiper.vue
@@ -8,7 +8,7 @@ function loadImages () {
   for (let i = 1; i <= 10; i++) {
     imageData.value.push({
       title: `image-${i}`,
-      link: 'https://www.baidu.com',
+      // link: '',
       imgUrl: getImageUrl(i)
     })
   }
