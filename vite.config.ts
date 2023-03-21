9299bbd7b9874dea194be2e242ba0d8c3f7150fd
lhdai <d_muses@163.com>
2023-03-21 16:16:00 +0800

    swiper

--- vite.config.ts
+++ vite.config.ts
@@ -51,13 +51,13 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue', 'vue-router'],
       output: {
-        format: 'es', // 默认es
+        format: 'es', // 默认es，可选 'amd' 'cjs' 'es' 'iife' 'umd' 'system'
         exports: 'named', // https://rollupjs.org/configuration-options/#output-exports
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-          'vue-router': 'VueRouter' // 引入vue-router全局变量，否则router.push将无法使用
-        }
+        // globals: {
+        //   vue: 'Vue',
+        //   'vue-router': 'VueRouter' // 引入vue-router全局变量，否则router.push将无法使用
+        // }
       }
     },
     /** 设置为 false 可以禁用最小化混淆，或是用来指定使用哪种混淆器。
