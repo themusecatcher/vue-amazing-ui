c74510db3e3206f1c2eadd075c030fe8dfd226ae
themusecatcher <themusecatcher@163.com>
2023-03-23 17:47:30 +0800

    chore: update demo

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -7,12 +7,18 @@
     <Progress :width="900" :percent="36" :strokeWidth="10" :showInfo="true" />
     <h2 class="mt30 mb10">完成进度条（width: 100% & percent: 100）</h2>
     <Progress width="100%" :percent="100" :strokeWidth="10" :showInfo="true" />
-    <h2 class="mt30 mb10">渐变进度条</h2>
-    <Progress :width="900" :percent="60" :strokeWidth="10" :strokeColor="{
+    <h2 class="mt30">渐变进度条</h2>
+    <h3 class="mb10">strokeColor: { '0%': '#108ee9', '100%': '#87d068', direction: 'right' } 或 { from: '#108ee9', to: '#87d068', direction: 'right' }</h3>
+    <Progress
+      :width="900"
+      :percent="60"
+      :strokeWidth="10"
+      :strokeColor="{
         '0%': '#108ee9',
         '100%': '#87d068',
         direction: 'right'
-      }" :showInfo="true" />
+      }"
+      :showInfo="true" />
   </div>
 </template>
 <style lang="less" scoped>
