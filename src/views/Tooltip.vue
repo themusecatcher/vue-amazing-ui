15829072d37cf923b4507eec3c3ecbae952883e6
themusecatcher <themusecatcher@163.com>
2023-03-24 17:39:32 +0800

    chore: update demo

--- src/views/Tooltip.vue
+++ src/views/Tooltip.vue
@@ -14,13 +14,12 @@ onMounted(() => {
 </script>
 <template>
   <div>
-    <p class="u-info mt100 mb10">普通用法：</p>
+    <p class="u-info mt150 mb10">普通用法：</p>
     <Tooltip :maxWidth="240">
       <template #title>特斯拉(Tesla)是美国一家电动汽车及能源公司，总部位于帕洛阿托(Palo Alto)，市值达2100亿美元，产销电动汽车、太阳能板、及储能设备</template>
       <div class="u-tag yellow">特斯拉</div>
     </Tooltip>
-    <br/>
-    <p class="u-info mt100 mb10">模拟接口调用：</p>
+    <p class="u-info ml120 mb10">模拟接口调用：</p>
     <Tooltip :maxWidth="240">
       <!-- <template #title>《哥斯拉大战金刚》是由美国传奇影业公司出品，亚当·温佳德执导，亚历山大·斯卡斯加德、米莉·博比·布朗、丽贝卡·豪尔、凯莉·霍特尔、布莱恩·泰里·亨利、小栗旬联合主演的动作科幻片，于2021于3月26日在中国内地上映</template> -->
       <template #title>{{ title || '--' }}</template>
@@ -46,12 +45,6 @@ onMounted(() => {
 .u-info {
   display: inline-block;
   font-size: 16px;
-}
-.mt100 {
-  margin-top: 100px;
-}
-.mr20 {
-  margin-right: 20px;
 }
 .yellow {
   color: #FF9000;
