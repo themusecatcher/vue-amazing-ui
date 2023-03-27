b864afa63b40459606d26f3c58e95fe75310c35d
lhdai <d_muses@163.com>
2023-03-27 15:20:46 +0800

    message

--- packages/message/Message.vue
+++ packages/message/Message.vue
@@ -96,39 +96,37 @@ function onHideMessage (index: number) {
 </script>
 <template>
   <div class="m-message-wrap" :style="`top: ${top}px;`">
-    <div class="m-message" v-for="(message, index) in messageContent" :key="index">
-      <Transition name="slide-fade" :key="index">
-        <div class="m-message-content" @mouseenter="onEnter(index)" @mouseleave="onLeave(index)" v-if="showMessage[index]">
+    <TransitionGroup name="slide-fade">
+      <div class="m-message" v-for="(message, index) in messageContent" :key="index">
+        <div class="m-message-content" v-show="showMessage[index]" @mouseenter="onEnter(index)" @mouseleave="onLeave(index)">
           <svg class="svg" v-if="message.mode==='info'" :style="{fill: ColorStyle[message.mode] }" viewBox="64 64 896 896" data-icon="info-circle" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
           <svg class="svg" v-if="message.mode==='success'" :style="{fill: ColorStyle[message.mode] }" viewBox="64 64 896 896" data-icon="check-circle" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 0 1-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"></path></svg>
           <svg class="svg" v-if="message.mode==='error'" :style="{fill: ColorStyle[message.mode] }" viewBox="64 64 896 896" data-icon="close-circle" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path></svg>
           <svg class="svg" v-if="message.mode==='warn'" :style="{fill: ColorStyle[message.mode] }" viewBox="64 64 896 896" data-icon="exclamation-circle" aria-hidden="true" focusable="false"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
           <p class="content">{{ message.content }}</p>
         </div>
-      </Transition>
-    </div>
+      </div>
+    </TransitionGroup>
   </div>
 </template>
 <style lang="less" scoped>
-// 渐变过渡效果
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .3s;
-}
-.fade-enter-form, .fade-leave-to {
-  opacity: 0;
-}
 // 滑动渐变过渡效果
-.slide-fade-enter-active {
-  transition: all .5s ease;
-}
+.slide-fade-move,
+.slide-fade-enter-active,
 .slide-fade-leave-active {
-  transition: all .5s ease;
+  transition: transform .3s ease, opacity .3s;
 }
 .slide-fade-enter-from, .slide-fade-leave-to {
   transform: translateY(-16px);
   -ms-transform: translateY(-16px); /* IE 9 */
   -webkit-transform: translateY(-16px); /* Safari and Chrome */
   opacity: 0;
+}
+.slide-fade-leave-active {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
 }
 .m-message-wrap {
   position: fixed;
