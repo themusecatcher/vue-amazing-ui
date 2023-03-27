0fb0798728d56428a55232719ec1e791f723f6bd
themusecatcher <themusecatcher@163.com>
2023-03-27 15:05:15 +0800

    chore: update Progress

--- packages/progress/Progress.vue
+++ packages/progress/Progress.vue
@@ -59,8 +59,8 @@ const backgroundColor = computed(() => {
   </div>
   <div v-else class="m-progress-circle" :style="`width: ${totalWidth}; height: ${totalWidth};`">
     <svg class="u-progress-circle" viewBox="0 0 100 100">
-      <path :d="path" stroke-linecap="round" :stroke-width="strokeWidth" :style="`stroke-dasharray: ${perimeter}px, ${perimeter}px;`" fill-opacity="0" class="u-progress-circle-trail"></path>
-      <path :d="path" stroke-linecap="round" :stroke-width="strokeWidth" :style="`stroke-dasharray: ${(percent / 100) * perimeter}px, ${perimeter}px;`" :opacity="percent === 0 ? 0:1" fill-opacity="0" class="u-progress-circle-path" :class="{success: percent >= 100}"></path>
+      <path :d="path" stroke-linecap="round" class="u-progress-circle-trail" :stroke-width="strokeWidth" :style="`stroke-dasharray: ${perimeter}px, ${perimeter}px;`" fill-opacity="0"></path>
+      <path :d="path" stroke-linecap="round" class="u-progress-circle-path" :class="{success: percent >= 100}" :stroke-width="strokeWidth" :style="`stroke: ${backgroundColor}; stroke-dasharray: ${(percent / 100) * perimeter}px, ${perimeter}px;`" :opacity="percent === 0 ? 0:1" fill-opacity="0"></path>
     </svg>
     <template v-if="showInfo">
       <svg class="u-success" v-if="percent>=100" focusable="false" data-icon="check" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path></svg>
@@ -133,7 +133,7 @@ const backgroundColor = computed(() => {
                   opacity .3s;
     }
     .success {
-      stroke: @success;
+      stroke: @success !important;
     }
   }
   .u-success {
