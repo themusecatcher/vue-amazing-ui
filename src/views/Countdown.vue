d586ae8554b2da8fdf23e7ae3d50e16a1bc2a1f7
lhdai <d_muses@163.com>
2023-03-30 11:14:09 +0800

    countdown

--- src/views/Countdown.vue
+++ src/views/Countdown.vue
@@ -6,11 +6,11 @@ function onFinish () {
 
 <template>
   <div>
-    <h2 class="mb10">Countdown 倒计时基本使用（format: Y 年 M 月 D 天 H 时 m 分 s 秒 SSS 毫秒）</h2>
+    <h2 class="mb10">Countdown 倒计时基本使用（format: Y 年 M 月 D 天 H 时 m 分 s 秒）</h2>
     <Countdown
       title="Countdown 1年"
       :countdown="12 * 30 * 24 * 60 * 60 * 1000"
-      format="Y 年 M月 D 天 H 时 m 分 s 秒 SSS 毫秒"
+      format="MM月D天 HH:mm:ss"
       finishedText="Finished"
       @finish="onFinish">
       <template #prefix>There's only </template>
@@ -28,6 +28,13 @@ function onFinish () {
       <template #prefix>There's only </template>
       <!-- <template #finish>&lt; FinishedText slot &gt;</template> -->
       <template #suffix> left for the end.</template>
+    </CountDown>
+    <h2 class="mt30 mb10">倒计时已完成</h2>
+    <Countdown
+      title="Finished"
+      format="Y 年 M 月 D 天 H 时 m 分 s 秒 SSS"
+      finishedText="Finished"
+      @finish="onFinish">
     </CountDown>
   </div>
 </template>
