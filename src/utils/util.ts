b8b3475ab8627263c067fd7fab30a681d9360d04
lhdai <d_muses@163.com>
2023-03-31 10:01:51 +0800

    dateFormat

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,63 +1,43 @@
-// var timestamp = 1626056435
-// console.log('dateFormat:', dateFormat(1626056435, 'YYYYMMDD HH:mm:ss', '/')) // 2021/07/12 10:20:35
-// console.log('dateFormat:', dateFormat(1626056435, 'YYYYMMDD HH:mm:ss', '', 'CN')) // 2021年07月12日 10时20分35秒
-// 默认format：YYYYMMDD，默认分隔符seq：'-'，默认格式类型lang：'US'
-export function dateFormat (timestamp: number, format = 'YYYYMMDD', seq = '-', lang = 'US'): number|string {
-  if (!isFinite(timestamp)) return '--'
-  if (String(timestamp).length === 10) {
-    timestamp = timestamp * 1000
+/*
+  timestamp: 13位时间戳 | new Date() | Date()
+  console.log(dateFormat(1680227496788, 'YYYY-MM-DD HH:mm:ss'))
+  format => YY：年，M：月，D：日，H：时，m：分钟，s：秒，SSS：毫秒
+*/
+export function dateFormat (timestamp: number|string|Date, format = 'YYYY-MM-DD HH:mm:ss'): string {
+  var date = new Date(timestamp)
+  function fixedTwo (value: number): string {
+    return value < 10 ? '0' + value : String(value)
   }
-  let date = new Date(timestamp)
-  let Y: any = date.getFullYear()
-  let M: any = date.getMonth() + 1
-  M = M < 10 ? '0' + M : M
-  let D: any = date.getDate()
-  D = D < 10 ? '0' + D : D
-  let hour: any = date.getHours()
-  hour = hour < 10 ? '0' + hour : hour
-  let min: any = date.getMinutes()
-  min = min < 10 ? '0' + min : min
-  let sec: any = date.getSeconds()
-  sec = sec < 10 ? '0' + sec : sec
-  if (lang === 'CN') {
-    Y += '年'
-    M += '月'
-    D += '日'
-    hour += '时'
-    min += '分'
-    sec += '秒'
-    if (format === 'YYYY') {
-      return Y // 2021年
-    } else if (format === 'YYYYMM') { // 2021年07月
-      return `${Y} + ${M}`
-    } else if (format === 'YYYYMMDD') { // 2021年07月12日
-      return Y + M + D
-    } else if (format === 'HH:mm:ss') { // 10时20分35秒
-      return hour + min + sec
-    } else if (format === 'YYYYMMDD HH:mm') { // 2021年07月12日 10时20分
-      return Y + M + D + ' ' + hour + ':' + min
-    } else if (format === 'YYYYMMDD HH:mm:ss') { // 2021年07月12日 10时20分35秒
-      return Y + M + D + ' ' + hour + min + sec
-    } else {
-      return '--'
-    }
-  } else {
-    if (format === 'YYYY') {
-      return Y // 2021
-    } else if (format === 'YYYYMM') { // 2021-07
-      return Y + seq + M
-    } else if (format === 'YYYYMMDD') { // 2021-07-12
-      return Y + seq + M + seq + D
-    } else if (format === 'HH:mm:ss') { // 10:20:35
-      return hour + ':' + min + ':' + sec
-    } else if (format === 'YYYYMMDD HH:mm') { // 2021-07-12 10:20
-      return Y + seq + M + seq + D + ' ' + hour + ':' + min
-    } else if (format === 'YYYYMMDD HH:mm:ss') { // 2021-07-12 10:20:35
-      return Y + seq + M + seq + D + ' ' + hour + ':' + min + ':' + sec
-    } else {
-      return '--'
-    }
+  var showTime = format
+  if (showTime.includes('SSS')) {
+    const S = date.getMilliseconds()
+    showTime = showTime.replace('SSS', '0'.repeat(3 - String(S).length) + S)
   }
+  if (showTime.includes('YY')) {
+    const Y = date.getFullYear()
+    showTime = showTime.includes('YYYY') ? showTime.replace('YYYY', String(Y)) : showTime.replace('YY', String(Y).slice(2, 4))
+  }
+  if (showTime.includes('M')) {
+    const M = date.getMonth() + 1
+    showTime = showTime.includes('MM') ? showTime.replace('MM', fixedTwo(M)) : showTime.replace('M', String(M))
+  }
+  if (showTime.includes('D')) {
+    const D = date.getDate()
+    showTime = showTime.includes('DD') ? showTime.replace('DD', fixedTwo(D)) : showTime.replace('D', String(D))
+  }
+  if (showTime.includes('H')) {
+    const H = date.getHours()
+    showTime = showTime.includes('HH') ? showTime.replace('HH', fixedTwo(H)) : showTime.replace('H', String(H))
+  }
+  if (showTime.includes('m')) {
+    var m = date.getMinutes()
+    showTime = showTime.includes('mm') ? showTime.replace('mm', fixedTwo(m)) : showTime.replace('m', String(m))
+  }
+  if (showTime.includes('s')) {
+    var s = date.getSeconds()
+    showTime = showTime.includes('ss') ? showTime.replace('ss', fixedTwo(s)) : showTime.replace('s', String(s))
+  }
+  return showTime
 }
 // 获取静态资源地址
 export function getImageUrl (name: any) {
