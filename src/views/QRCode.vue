6457a2f503c42df8be67e74ec039e36aefd0fb3d
themusecatcher <themusecatcher@163.com>
2023-04-08 21:22:29 +0800

    chore: update demo

---
+++ src/views/QRCode.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div>
+  <h2 class="mb10">QRCode 二维码基本使用</h2>
+  <QRCode
+    value="https://blog.csdn.net/Dandrose"
+    :size="180"
+    :bordered="true"
+    color="#000"
+    backgroundColor="#FFF"
+    :scale="8"
+    errorLevel="H"/>
+  </div>
+</template>
+<style lang="less" scoped>
+</style>
