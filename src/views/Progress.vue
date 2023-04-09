7c28bd77f1220414309b826c81a525c9c40e2863
themusecatcher <themusecatcher@163.com>
2023-04-09 12:44:44 +0800

    chore: update demo

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -23,11 +23,11 @@ console.log(Math.PI)
 </script>
 <template>
   <div>
-    <h2 class="mb10">Progress 进度条基本使用（type: line）</h2>
+    <h2 class="mb10">Progress 进度条基本使用（width: 900 & type: line）</h2>
     <Progress :width="900" :percent="percent" :strokeWidth="10" :showInfo="true" />
     <h2 class="mt30 mb10">完成进度条（width: 100% & percent: 100）</h2>
     <Progress width="100%" :percent="100" :strokeWidth="10" :showInfo="true" />
-    <h2 class="mt30">渐变进度条</h2>
+    <h2 class="mt30">渐变进度条（width: 900）</h2>
     <h3 class="mb10">strokeColor: { '0%': '#108ee9', '100%': '#87d068', direction: 'right' } 或 { from: '#108ee9', to: '#87d068', direction: 'right' }</h3>
     <Progress
       :width="900"
