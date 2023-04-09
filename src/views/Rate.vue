822fd2db383f35cb823386445b4a9d0d9d58955a
lhdai <d_muses@163.com>
2023-04-09 12:40:15 +0800

    rate

---
+++ src/views/Rate.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const value = ref(2.99)
+const desc = ref(['terrible', 'bad', 'normal', 'good', 'wonderful'])
+watch(value, (to) => {
+  console.log('p to:', to)
+})
+function onChange (value: number) {
+  console.log('change value:', value)
+}
+function onHoverChange (value: number) {
+  console.log('hover value:', value)
+}
+</script>
+<template>
+  <div>
+    <h2 class="mb10">Rate 评分基本使用（character: star）</h2>
+    <Rate
+      :allowClear="false"
+      :allowHalf="true"
+      :count="5"
+      :tooltips="desc"
+      character="star"
+      :disabled="false"
+      v-model:value="value"
+      @change="onChange"
+      @hoverChange="onHoverChange"/>
+    <h2 class="mt30 mb10">Rate （character: heart & size: 30）</h2>
+    <Rate
+      :allowClear="true"
+      :allowHalf="true"
+      :count="5"
+      :tooltips="desc"
+      character="heart"
+      :size="30"
+      :disabled="false"
+      v-model:value="value"
+      @change="onChange"
+      @hoverChange="onHoverChange"/>
+    <h2 class="mt30 mb10">Rate 评分基本使用（character: 好 & size: 30）</h2>
+    <Rate
+      :allowClear="false"
+      :allowHalf="true"
+      :count="5"
+      :tooltips="desc"
+      character="好"
+      :size="30"
+      :disabled="false"
+      v-model:value="value"
+      @change="onChange"
+      @hoverChange="onHoverChange"/>
+    <h2 class="mt30 mb10">Ant Design Vue 评分</h2>
+    <a-rate v-model:value="value" :tooltips="desc" @change="onChange" />
+  </div>
+</template>
+<style lang="less" scoped>
+.u1 {
+  font-size: 16px;
+}
+</style>
