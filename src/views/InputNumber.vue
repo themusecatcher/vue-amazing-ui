876e8ab8e81a8bb452a0ec03563f6f5a96b78e78
themusecatcher <themusecatcher@163.com>
2023-04-09 12:44:59 +0800

    chore: update demo

---
+++ src/views/InputNumber.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const value = ref(3)
+watch(value, (to) => {
+  console.log('to:', to)
+})
+
+function onChange (number: number) {
+  console.log('number:', number)
+}
+function onAntChange (number: number) {
+  console.log('ant:', number)
+}
+</script>
+<template>
+  <div>
+    <h2 class="mb10">InputNumber 数字输入框基本使用</h2>
+    <InputNumber
+      :min="-10"
+      :max="10"
+      :step="1"
+      prefix="$"
+      @change="onChange"
+      v-model:value="value">
+    </InputNumber>
+    <h2 class="mt30 mb10">Ant Design Vue 数字输入框</h2>
+    <a-input-number
+      class="border-box"
+      :min="-10"
+      :max="10"
+      :step="1"
+      @change="onAntChange"
+      v-model:value="value" />
+  </div>
+</template>
+<style lang="less" scoped>
+.border-box {
+  :deep(*) {
+    box-sizing: border-box;
+  }
+}
+</style>
