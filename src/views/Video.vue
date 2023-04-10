5fc8584d880d564a05c4db5796f93f9c8fd525a1
themusecatcher <themusecatcher@163.com>
2023-04-10 16:38:12 +0800

    chore: update demo

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { rafTimeout } from '../../packages'
-// import videoUrl from '@/assets/files/Bao.mp4'
-// import videoCover from '@/assets/images/Bao.jpg'
+// import videoSrc from '@/assets/files/Bao.mp4'
+// import videoPoster from '@/assets/images/Bao.jpg'
 
-// const videoUrl = ref(new URL('@/assets/files/Bao.mp4', import.meta.url).href)
-// const videoCover = ref(new URL('@/assets/images/Bao.jpg', import.meta.url).href)
+// const videoSrc = ref(new URL('@/assets/files/Bao.mp4', import.meta.url).href)
+// const videoPoster = ref(new URL('@/assets/images/Bao.jpg', import.meta.url).href)
 
-const videoUrl = ref()
-const videoCover = ref()
+const videoSrc = ref('https://download.jinhui365.cn/group1/M00/01/2E/CgABcmQzxoKALxVFFreS-wnP-Ms443.mp4')
+const videoPoster = ref()
 
 onMounted(() => {
-  rafTimeout(() => { // 模拟接口调用
-      videoUrl.value = 'https://download.jinhui365.cn/group1/M00/00/A7/CgAAcmNCagWAejQND0jqHviL8QA869.mp4'
-      videoCover.value = 'https://download.jinhui365.cn//group1/M00/00/A8/CgAAcmNCddKACHUbAADB2zx3w90256.jpg'
-    }, 1000)
+  // rafTimeout(() => { // 模拟接口调用
+  //     videoSrc.value = 'https://download.jinhui365.cn/group1/M00/00/A7/CgAAcmNCagWAejQND0jqHviL8QA869.mp4'
+  //     videoPoster.value = 'https://download.jinhui365.cn//group1/M00/00/A8/CgAAcmNCddKACHUbAADB2zx3w90256.jpg'
+  //   }, 1000)
 })
 </script>
 
@@ -23,15 +23,15 @@ onMounted(() => {
     <h2 class="mb10">Video 播放器基本使用</h2>
     <Video
       v-show="true"
-      :videoUrl="videoUrl"
-      :videoCover="videoCover"
+      :videoSrc="videoSrc"
+      :videoPoster="videoPoster"
       :width="800"
       :height="450"
       :autoplay="false"
       :controls="true"
       :loop="false"
       :muted="false"
-      preload="auto"
+      preload="metadata"
       :showPlay="true"
       :playWidth="96"
       zoom="cotain" />
