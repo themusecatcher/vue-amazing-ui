586691b07da65a6b45b2493fb20c598b4ca9befc
lhdai <d_muses@163.com>
2023-04-10 11:05:31 +0800

    empty

---
+++ src/views/Empty.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>
+    <h2 class="mb10">Empty 空状态基本使用 (image: 1)</h2>
+    <Empty
+      image="1"
+      description="暂无数据"/>
+    <h2 class="mt30 mb10">Empty (image: 2)</h2>
+    <Empty
+      image="2"
+      description="暂无数据"/>
+    <h2 class="mt30 mb10">Empty (image: '图片地址' & image-style: {width: '100px', height: '60px'})</h2>
+    <Empty
+      image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+      :image-style="{
+        width: '100px',
+        height: '60px',
+      }">
+      <template #description>
+        <span>
+          Customize
+          <a href="#API">Description</a>
+        </span>
+      </template>
+    </Empty>
+  </div>
+</template>
+<style lang="less" scoped>
+
+</style>
