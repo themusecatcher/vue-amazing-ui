85b90d138b7d562c8bd29acf6b8f33403b7e470e
themusecatcher <themusecatcher@163.com>
2023-04-13 11:23:05 +0800

    chore: update

--- packages/alert/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import Alert from './Alert.vue'
-
-// 使用install方法，在app.use挂载
-Alert.install = (app: App): void => {
-  app.component(Alert.__name as string, Alert)
-}
-
-export default Alert
