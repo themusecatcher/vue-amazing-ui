6891068d549adb9c7ac45fd04df3063e1ca4510b
lhdai <d_muses@163.com>
2023-04-18 16:25:35 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@ const activeKey = ref(0)
     <ul class="m-list">
       <li class="u-tip mb10 mt10"> (yarn add vue-amazing-ui)安装组件库后，全局引入所有组件</li>
       <li class="u-tip mb10"> (yarn add vue-amazing-ui)安装组件库后，按需引入部分组件</li>
-      <li class="u-tip mb10"> git clone<a href="https://github.com/themusecatcher/vue-amazing-ui" target="_blank">组件库项目</a>到本地后，从packages下单独拷贝某个单文件组件 (SFC)到需要组件的项目内使用。</li>
+      <li class="u-tip mb10"> git clone <a href="https://github.com/themusecatcher/vue-amazing-ui" target="_blank">组件库项目</a> 到本地后，从packages下单独拷贝某个单文件组件 (SFC)到需要组件的项目内使用。</li>
     </ul>
     <h2 class="mt30">除此以外，Vue Amazing UI 还提供了八个常用工具函数 ：</h2>
     <ul class="m-list">
