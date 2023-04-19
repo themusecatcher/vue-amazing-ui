861bc2606a40540da668db52915a56e67d8e4c97
lhdai <d_muses@163.com>
2023-04-19 11:17:42 +0800

    code

--- src/views/CodeMirror.vue
+++ src/views/CodeMirror.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const code = ref(`<script setup lang="ts">
+import { dateFormat, requestAnimationFrame, cancelAnimationFrame, rafTimeout, cancelRaf, throttle, debounce, add } from 'vue-amazing-ui'
+<\/script>
+`)
 function onChange (value: string, viewUpdate: any) {
   console.log('change:', value)
   console.log('change:', viewUpdate)
@@ -16,7 +21,10 @@ function onBlur (viewUpdate: any) {
     <h1>vue-codemirror 参考文档</h1>
     <ul class="m-list">
       <li>
-        <a class="u-file" href="https://codemirror.net/" target="_blank">CodeMirror官网</a>
+        <a class="u-file" href="https://codemirror.net/" target="_blank">CodeMirror 官网</a>
+      </li>
+      <li>
+        <a class="u-file" href="https://codemirror.net/docs/ref/" target="_blank">CodeMirror6 APIs</a>
       </li>
       <li>
         <a class="u-file" href="https://github.com/orgs/codemirror/repositories?q=lang-&type=all" target="_blank">CodeMirror6 Languages</a>
@@ -27,7 +35,9 @@ function onBlur (viewUpdate: any) {
     </ul>
     <h2 class="mt30 mb10">CodeMirror</h2>
     <CodeMirror
-      :codeStyle="{ width: '1000px', height: '400px', fontSize: '16px' }"
+      v-model:code="code"
+      dark
+      :codeStyle="{ width: '1000px', height: '500px', fontSize: '16px' }"
       @change="onChange"
       @focus="onFocus"
       @blur="onBlur" />
