f36a36d6e98813c225320a0d10a65ff41df30276
lhdai <d_muses@163.com>
2023-04-27 11:02:21 +0800

    update

--- script/publish.sh
+++ script/publish.sh
@@ -1,7 +1,7 @@
 # /bin/bash
 version=`jq -r .version ../package.json` # 读取package.json中的version
 yarn build # 打包构建
-pnpm publish ../ # 发布到npm
+pnpm publish ../ # 发布到npm，pnpm(高性能的npm)
 git add ../
 git commit -m "update ${version}"
 git push # 提交项目代码到github
