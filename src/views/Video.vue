b835255aff215e08b36f2c59808aef19a7da5d7c
themusecatcher <themusecatcher@163.com>
2023-05-17 10:33:01 +0800

    chore: update Video

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -2,18 +2,18 @@
 import { ref, onMounted } from 'vue'
 import { rafTimeout } from '../../packages'
 // import videoSrc from '@/assets/files/Bao.mp4'
-// import videoPoster from '@/assets/images/Bao.jpg'
+// import videoPoster from '@/assets/images/bao.jpg'
 
 // const videoSrc = ref(new URL('@/assets/files/Bao.mp4', import.meta.url).href)
-// const videoPoster = ref(new URL('@/assets/images/Bao.jpg', import.meta.url).href)
+// const videoPoster = ref(new URL('@/assets/images/bao.jpg', import.meta.url).href)
 
-const videoSrc = ref('https://download.jinhui365.cn/group1/M00/01/2E/CgABcmQzxoKALxVFFreS-wnP-Ms443.mp4')
-const videoPoster = ref()
+const videoSrc = ref('https://cdn.jsdelivr.net/gh/themusecatcher/static-resources@0.0.2/Bao.mp4')
+const videoPoster = ref('https://cdn.jsdelivr.net/gh/themusecatcher/static-resources@0.0.2/bao.jpg')
 
 onMounted(() => {
   // rafTimeout(() => { // 模拟接口调用
-  //     videoSrc.value = 'https://download.jinhui365.cn/group1/M00/00/A7/CgAAcmNCagWAejQND0jqHviL8QA869.mp4'
-  //     videoPoster.value = 'https://download.jinhui365.cn//group1/M00/00/A8/CgAAcmNCddKACHUbAADB2zx3w90256.jpg'
+  //     videoSrc.value = 'https://cdn.jsdelivr.net/gh/themusecatcher/static-resources@0.0.2/Bao.mp4'
+  //     videoPoster.value = 'https://cdn.jsdelivr.net/gh/themusecatcher/static-resources@0.0.2/bao.jpg'
   //   }, 1000)
 })
 </script>
