3b78c5089a2e63497a648b1114bdbb89efd10079
themusecatcher <themusecatcher@163.com>
2023-05-30 18:11:15 +0800

    chore: update 0.0.26

--- script/publish.sh
+++ script/publish.sh
@@ -2,7 +2,7 @@
 version=`jq -r .version ../package.json` # 读取package.json中的version
 pnpm build # 打包构建
 cd ../
-pnpm publish # 发布到npm，pnpm(高性能的npm)
 git add .
 git commit -m "update ${version}"
 git push # 提交项目代码到github
+pnpm publish # 发布到npm，pnpm(高性能的npm)
