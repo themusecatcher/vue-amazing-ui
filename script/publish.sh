a225447d15a5f080a672d483554f40df3b4a3945
themusecatcher <themusecatcher@163.com>
2023-05-31 18:12:46 +0800

    chore: update

--- script/publish.sh
+++ script/publish.sh
@@ -2,7 +2,14 @@
 version=`jq -r .version ../package.json` # 读取package.json中的version
 pnpm build # 打包构建
 cd ../
+
+ # 提交项目代码到github
 git add .
-git commit -m "update ${version}"
-git push # 提交项目代码到github
-pnpm publish # 发布到npm，pnpm(高性能的npm)
+git commit -m "update $version"
+git push
+
+ # 发布到npm，pnpm(高性能的npm)
+pnpm publish
+
+# 升级 vue-amazing-ui 依赖版本
+pnpm up vue-amazing-ui@$version
