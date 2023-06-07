556f485b8dc9f95e0741c74b168531f483ddfaa8
themusecatcher <themusecatcher@163.com>
2023-06-07 10:20:14 +0800

    chore: update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import './global.less'
+import './global.less' // global less
 import VueAmazingUI from 'vue-amazing-ui'
 import 'vue-amazing-ui/css'
 // import VueAmazingUI from '../../../dist/vue-amazing-ui'
