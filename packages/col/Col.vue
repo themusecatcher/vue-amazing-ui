c94b5f6e295fcce8f1cbb60e2cc3bb532122ece3
themusecatcher <themusecatcher@163.com>
2023-07-14 17:49:16 +0800

    chore: update

--- packages/col/Col.vue
+++ packages/col/Col.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 interface Props {
-  span?: number // 栅格占位格数，为 0 时相当于 display: none
+  span?: number // 栅格占位格数，为 0 时相当于 display: none，取0,1,2...24
+  offset?: number // 栅格左侧的间隔格数，取0,1,2...24
 }
 withDefaults(defineProps<Props>(), {
-  span: 0
+  span: undefined,
+  offset: 0
 })
 </script>
 <template>
-  <div :class="`m-col col-${span}`" style="padding-left: var(--xGap); padding-right: var(--xGap);">
+  <div :class="`m-col col-${span} offset-${offset}`" style="padding-left: var(--xGap); padding-right: var(--xGap);">
     <slot></slot>
   </div>
 </template>
@@ -20,124 +22,199 @@ withDefaults(defineProps<Props>(), {
   color: rgba(0, 0, 0, 0.88);
   line-height: 1.5714285714285714;
 }
+.col-0 {
+  display: none;
+}
 .col-1 {
   display: block;
   flex: 0 0 4.16666666666666%;
   max-width: 4.16666666666666%;
+}
+.offset-1 {
+  margin-inline-start: 4.16666666666666%;
 }
 .col-2 {
   display: block;
   flex: 0 0 8.33333333333333%;
   max-width: 8.33333333333333%;
 }
+.offset-2 {
+  margin-inline-start: 8.33333333333333%;
+}
 .col-3 {
   display: block;
   flex: 0 0 12.5%;
   max-width: 12.5%;
+}
+.offset-3 {
+  margin-inline-start: 12.5%;
 }
 .col-4 {
   display: block;
   flex: 0 0 16.66666666666666%;
   max-width: 16.66666666666666%;
 }
+.offset-4 {
+  margin-inline-start: 16.66666666666666%;
+}
 .col-5 {
   display: block;
   flex: 0 0 20.83333333333333%;
   max-width: 20.83333333333333%;
+}
+.offset-5 {
+  margin-inline-start: 20.83333333333333%;
 }
 .col-6 {
   display: block;
   flex: 0 0 25%;
   max-width: 25%;
 }
+.offset-6 {
+  margin-inline-start: 25%;
+}
 .col-7 {
   display: block;
   flex: 0 0 29.16666666666666%;
   max-width: 29.16666666666666%;
+}
+.offset-7 {
+  margin-inline-start: 29.16666666666666%;
 }
 .col-8 {
   display: block;
   flex: 0 0 33.33333333333333%;
   max-width: 33.33333333333333%;
 }
+.offset-8 {
+  margin-inline-start: 33.33333333333333%;
+}
 .col-9 {
   display: block;
   flex: 0 0 37.5%;
   max-width: 37.5%;
+}
+.offset-9 {
+  margin-inline-start: 37.5%;
 }
 .col-10 {
   display: block;
   flex: 0 0 41.66666666666666%;
   max-width: 41.66666666666666%;
 }
+.offset-10 {
+  margin-inline-start: 41.66666666666666%;
+}
 .col-11 {
   display: block;
   flex: 0 0 45.83333333333333%;
   max-width: 45.83333333333333%;
+}
+.offset-11 {
+  margin-inline-start: 45.83333333333333%;
 }
 .col-12 {
   display: block;
   flex: 0 0 50%;
   max-width: 50%;
 }
+.offset-12 {
+  margin-inline-start: 50%;
+}
 .col-13 {
   display: block;
   flex: 0 0 54.16666666666666%;
   max-width: 54.16666666666666%;
+}
+.offset-13 {
+  margin-inline-start: 54.16666666666666%;
 }
 .col-14 {
   display: block;
   flex: 0 0 58.33333333333333%;
   max-width: 58.33333333333333%;
 }
+.offset-14 {
+  margin-inline-start: 58.33333333333333%;
+}
 .col-15 {
   display: block;
   flex: 0 0 62.5%;
   max-width: 62.5%;
+}
+.offset-15 {
+  margin-inline-start: 62.5%;
 }
 .col-16 {
   display: block;
   flex: 0 0 66.66666666666666%;
   max-width: 66.66666666666666%;
 }
+.offset-16 {
+  margin-inline-start: 66.6666666666666%;
+}
 .col-17 {
   display: block;
   flex: 0 0 70.83333333333333%;
   max-width: 70.83333333333333%;
+}
+.offset-17 {
+  margin-inline-start: 70.83333333333333%;
 }
 .col-18 {
   display: block;
   flex: 0 0 75%;
   max-width: 75%;
 }
+.offset-18 {
+  margin-inline-start: 75%;
+}
 .col-19 {
   display: block;
   flex: 0 0 79.16666666666666%;
   max-width: 79.16666666666666%;
+}
+.offset-19 {
+  margin-inline-start: 79.16666666666666%;
 }
 .col-20 {
   display: block;
   flex: 0 0 83.33333333333333%;
   max-width: 83.33333333333333%;
 }
+.offset-20 {
+  margin-inline-start: 83.33333333333333%;
+}
 .col-21 {
   display: block;
   flex: 0 0 87.5%;
   max-width: 87.5%;
+}
+.offset-21 {
+  margin-inline-start: 87.5%;
 }
 .col-22 {
   display: block;
   flex: 0 0 91.66666666666666%;
   max-width: 91.66666666666666%;
 }
+.offset-22 {
+  margin-inline-start: 91.66666666666666%;
+}
 .col-23 {
   display: block;
   flex: 0 0 95.83333333333333%;
   max-width: 95.83333333333333%;
 }
+.offset-23 {
+  margin-inline-start: 95.83333333333333%;
+}
 .col-24 {
   display: block;
   flex: 0 0 100%;
   max-width: 100%;
+}
+.offset-24 {
+  margin-inline-start: 100%;
 }
 </style>
--- src/views/Message.vue
+++ src/views/Message.vue
@@ -2,16 +2,16 @@
 import { ref } from 'vue'
 const message = ref()
 
-function onInfo (content: any) {
+function onInfo (content: string) {
   message.value.info(content) // info调用
 }
-function onSuccess (content: any) {
+function onSuccess (content: string) {
   message.value.success(content) // success调用
 }
-function onError (content: any) {
+function onError (content: string) {
   message.value.error(content) // error调用
 }
-function onWarn (content: any) {
+function onWarn (content: string) {
   message.value.warn(content) // warn调用
 }
 function onClose () {
@@ -22,10 +22,12 @@ function onClose () {
   <div>
     <h1>Message 全局提示</h1>
     <h2 class="mt30 mb10">基本使用</h2>
-    <Button type="primary" @click="onInfo('This is a normal message')" class="mr30">Info</Button>
-    <Button type="primary" @click="onSuccess('This is a success message')" class="mr30">Success</Button>
-    <Button type="primary" @click="onError('This is a error message')" class="mr30">Error</Button>
-    <Button type="primary" @click="onWarn('This is a warn message')" class="mr30">Warn</Button>
+    <Space :size="30">
+      <Button type="primary" @click="onInfo('This is a normal message')">Info</Button>
+      <Button type="primary" @click="onSuccess('This is a success message')">Success</Button>
+      <Button type="primary" @click="onError('This is a error message')">Error</Button>
+      <Button type="primary" @click="onWarn('This is a warn message')">Warn</Button>
+    </Space>
     <Message ref="message" :duration="3000" :top="30" @close="onClose" />
   </div>
 </template>
