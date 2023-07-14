5546d740b1afe1cf597f1dab4f8aaae38b63aabe
lhdai <d_muses@163.com>
2023-07-14 17:49:16 +0800

    update

--- packages/row/Row.vue
+++ packages/row/Row.vue
@@ -10,7 +10,7 @@ interface Responsive {
 }
 interface Props {
   // 推荐使用 (16+8n)px 作为栅格间隔(n 是自然数：0,1,2,3...)
-  gutter?: number|number[]|Responsive // 栅格间隔，可以写成像素值或支持响应式的对象写法来设置水平间隔 { xs: 8, sm: 16, md: 24}。或者使用数组形式同时设置 [水平间距, 垂直间距]
+  gutter?: number|[number|Responsive, number|Responsive]|Responsive // 栅格间隔，可以写成像素值或支持响应式的对象写法来设置水平间隔 { xs: 8, sm: 16, md: 24}。或者使用数组形式同时设置 [水平间距, 垂直间距]
   wrap?: boolean // 是否自动换行
 }
 const props = withDefaults(defineProps<Props>(), {
@@ -22,6 +22,26 @@ const xGap = computed(() => {
     return props.gutter
   }
   if (Array.isArray(props.gutter)) {
+    if (typeof props.gutter[0] === 'object') {
+      if (clientWidth.value >= 1600 && props.gutter[0].xxl) {
+        return props.gutter[0].xxl
+      }
+      if (clientWidth.value >= 1200 && props.gutter[0].xl) {
+        return props.gutter[0].xl
+      }
+      if (clientWidth.value >= 992 && props.gutter[0].lg) {
+        return props.gutter[0].lg
+      }
+      if (clientWidth.value >= 768 && props.gutter[0].md) {
+        return props.gutter[0].md
+      }
+      if (clientWidth.value >= 576 && props.gutter[0].sm) {
+        return props.gutter[0].sm
+      }
+      if (clientWidth.value < 576 && props.gutter[0].xs) {
+        return props.gutter[0].xs
+      }
+    }
     return props.gutter[0]
   }
   if (typeof props.gutter === 'object') {
@@ -48,6 +68,26 @@ const xGap = computed(() => {
 })
 const yGap = computed(() => {
   if (Array.isArray(props.gutter)) {
+    if (typeof props.gutter[1] === 'object') {
+      if (clientWidth.value >= 1600 && props.gutter[1].xxl) {
+        return props.gutter[1].xxl
+      }
+      if (clientWidth.value >= 1200 && props.gutter[1].xl) {
+        return props.gutter[1].xl
+      }
+      if (clientWidth.value >= 992 && props.gutter[1].lg) {
+        return props.gutter[1].lg
+      }
+      if (clientWidth.value >= 768 && props.gutter[1].md) {
+        return props.gutter[1].md
+      }
+      if (clientWidth.value >= 576 && props.gutter[1].sm) {
+        return props.gutter[1].sm
+      }
+      if (clientWidth.value < 576 && props.gutter[1].xs) {
+        return props.gutter[1].xs
+      }
+    }
     return props.gutter[1]
   }
   return 0
@@ -67,7 +107,7 @@ function getBrowserSize () {
 }
 </script>
 <template>
-  <div class="m-row" :class="{'gutter-row': gutter}" :style="`--xGap: ${xGap / 2}px; margin-left: -${xGap / 2}px; margin-right: -${xGap / 2}px; row-gap: ${yGap}px;`">
+  <div class="m-row" :class="{'gutter-row': gutter}" :style="`--xGap: ${(xGap as number) / 2}px; margin-left: -${(xGap as number) / 2}px; margin-right: -${(xGap as number) / 2}px; row-gap: ${yGap}px;`">
     <slot></slot>
   </div>
 </template>
@@ -79,19 +119,5 @@ function getBrowserSize () {
   font-size: 14px;
   color: rgba(0, 0, 0, 0.88);
   transition: all .3s;
-  :deep(.m-col) {
-    min-height: 30px;
-    margin-top: 8px;
-    margin-bottom: 8px;
-    color: #fff;
-    text-align: center;
-    border-radius: 0;
-  }
-}
-.gutter-row {
-  :deep(.m-col) {
-    margin-top: 0;
-    margin-bottom: 0;
-  }
 }
 </style>
--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -77,11 +77,56 @@
         <div class="gutter-box">col-6</div>
       </Col>
     </Row>
+    <h2 class="mt30 mb10">响应式垂直区块间隔</h2>
+    <Row :gutter="[16, { xs: 8, sm: 16, md: 24, lg: 32 }]">
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+      <Col class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </Col>
+    </Row>
+    <h2 class="mt30 mb10">左右偏移</h2>
+    <Row class="row">
+      <Col :span="8">col-8</Col>
+      <Col :span="8" :offset="8">col-8</Col>
+    </Row>
+    <Row class="row">
+      <Col :span="6" :offset="6">col-6 col-offset-6</Col>
+      <Col :span="6" :offset="6">col-6 col-offset-6</Col>
+    </Row>
+    <Row class="row">
+      <Col :span="12" :offset="6">col-12 col-offset-6</Col>
+    </Row>
   </div>
 </template>
 <style lang="less" scoped>
 .row {
   .m-col {
+    min-height: 30px;
+    margin-top: 8px;
+    margin-bottom: 8px;
+    color: #fff;
+    text-align: center;
+    border-radius: 0;
     padding: 16px 0;
     background: #1677ff;
   }
@@ -89,8 +134,22 @@
     background: #1677ffbf;
   }
 }
-.gutter-box {
-  background: #0092ff;
-  padding: 8px 0;
+.gutter-row {
+  .m-col {
+    min-height: 30px;
+    margin-top: 8px;
+    margin-bottom: 8px;
+    color: #fff;
+    text-align: center;
+    border-radius: 0;
+  }
+  .gutter-box {
+    background: #0092ff;
+    padding: 8px 0;
+  }
+  .m-col {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 }
 </style>
