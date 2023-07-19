4e6564c29d951e2dff6eb1d2570705693a54a042
themusecatcher <themusecatcher@163.com>
2023-07-19 09:24:17 +0800

    chore: update

--- packages/alert/Alert.vue
+++ packages/alert/Alert.vue
@@ -137,7 +137,7 @@ function onClose (e: MouseEvent):void {
       display: inline-block;
       vertical-align: bottom;
       fill: rgba(0, 0, 0, 0.45);
-      transition: color 0.2s;
+      transition: fill 0.2s;
       &:hover {
         fill: rgba(0, 0, 0, 0.88);
       }
