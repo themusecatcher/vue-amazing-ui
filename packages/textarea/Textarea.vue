94cf9aaad666a24ffa83c6069d13ec81bfd0177d
lhdai <d_muses@163.com>
2023-07-25 16:07:44 +0800

    update

--- packages/textarea/Textarea.vue
+++ packages/textarea/Textarea.vue
@@ -12,7 +12,7 @@ import { ref, computed, watch, onMounted, onUnmounted, nextTick } from 'vue'
 interface Props {
   width?: string|number // 输入框宽度
   allowClear?: boolean // 可以点击清除图标删除内容
-  autoSize?: boolean|{minRows:number, maxRows:number} // 自适应内容高度
+  autoSize?: boolean|{minRows?:number, maxRows?:number} // 自适应内容高度
   disabled?: boolean // 是否禁用
   maxlength?: number // 最大长度
   showCount?: boolean // 是否展示字数
@@ -35,6 +35,29 @@ const areaWidth = computed(() => {
   }
   return props.width
 })
+const autoSizeProperty = computed(() => {
+  if (typeof props.autoSize === 'object') {
+    const style: {'min-height'?: string, 'max-height'?: string, [propName: string]: any} = {
+      resize: 'none'
+    }
+    if ('minRows' in props.autoSize) {
+      style['min-height'] = (props.autoSize.minRows as number) * 22 + 10 + 'px'
+    }
+    if ('maxRows' in props.autoSize) {
+      style['max-height'] = (props.autoSize.maxRows as number) * 22 + 10 + 'px'
+    }
+    return style
+  }
+  if (typeof props.autoSize === 'boolean') {
+    if (props.autoSize) {
+      return {
+        'max-height': '9000000000000000px',
+        resize: 'none'
+      }
+    }
+    return {}
+  }
+})
 const showCountNum = computed(() => {
   if (props.maxlength) {
     return props.value.length + ' / ' + props.maxlength
@@ -43,61 +66,25 @@ const showCountNum = computed(() => {
 })
 watch(
   () => props.value,
-  (to) => {
-    // console.log('%O', textarea.value)
+  () => {
+    if (JSON.stringify(autoSizeProperty.value) !== '{}') {
+      areaHeight.value = 32
+      nextTick(() => {
+        getAreaHeight()
+      })
+    }
   }
 )
 const textarea = ref()
 const areaHeight = ref(32)
-const suffixRef = ref()
-const showSuffix = ref(1)
-const observer = ref()
 onMounted(() => {
-  showSuffix.value = suffixRef.value.offsetHeight
-  if (props.autoSize) {
-    // 观察器的配置（需要观察什么变动）
-    const config = { attributes: true, childList: false, subtree: false }
-    // 创建一个观察器实例并传入回调函数
-    observer.value = new MutationObserver(callback)
-    // 以上述配置开始观察目标节点
-    observer.value.observe(textarea.value, config)
-  }
   getAreaHeight()
 })
-onUnmounted(() => {
-  if (props.autoSize) {
-    // 之后，可停止观察
-    observer.value.disconnect()
-  }
-})
-/*
-  使用 MutationObserver 监听 textarea resize 时的属性变化
-  参考文档：https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver
-*/
-// 当观察到变动时执行的回调函数
-const callback = function (mutationsList: any, observer: any) {
-  console.log('mutation')
-  getAreaHeight()
-  // Use traditional 'for loops' for IE 11
-  for(let mutation of mutationsList) {
-    if (mutation.type === 'childList') {
-      console.log('A child node has been added or removed.')
-    }
-    if (mutation.type === 'attributes') {
-      console.log('The ' + mutation.attributeName + ' attribute was modified.')
-      console.log(mutation.target.style.height)
-    }
-  }
-}
 function getAreaHeight () {
   areaHeight.value = textarea.value.scrollHeight + 2
 }
 const emits = defineEmits(['update:value', 'change', 'enter'])
 function onInput (e: any) {
-  // if (props.autoSize) {
-  //   areaHeight.value = 32
-  //   getAreaHeight()
-  // }
   if (!('lazy' in props.valueModifiers)) {
     emits('update:value', e.target.value)
     emits('change', e)
@@ -114,19 +101,22 @@ function onKeyboard (e: any) {
     emits('enter', e)
   }
 }
-const input = ref()
 function onClear () {
   emits('update:value', '')
-  input.value.focus()
+  textarea.value.focus()
 }
 </script>
 <template>
-  <div class="m-textarea" :class="{disabled: disabled}" :style="`width: ${areaWidth};`">
+  <div
+    class="m-textarea"
+    :class="{'show-count': showCount}"
+    :style="`width: ${areaWidth};`"
+    :data-count="showCountNum">
     <textarea
       ref="textarea"
       class="u-textarea"
-      :class="{'auto-size': autoSize}"
-      :style="`height: ${areaHeight}px;`"
+      :class="{disabled: disabled}"
+      :style="[`height: ${autoSize ? areaHeight : ''}px`, autoSizeProperty]"
       :value="value"
       :maxlength="maxlength"
       :disabled="disabled"
@@ -134,11 +124,8 @@ function onClear () {
       @change="onChange"
       @keydown="onKeyboard"
       v-bind="$attrs" />
-    <span class="m-suffix" ref="suffixRef" v-if="showSuffix">
-      <span class="m-action" v-if="!disabled&&allowClear&&value" @click="onClear">
-        <svg focusable="false" class="u-clear" data-icon="close-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 01-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path></svg>
-      </span>
-      <span class="m-count" v-if="showCount">{{ showCountNum }}</span>
+    <span class="m-clear" v-if="!disabled&&allowClear&&value" @click="onClear">
+      <svg focusable="false" class="u-clear" data-icon="close-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 01-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path></svg>
     </span>
   </div>
 </template>
@@ -156,6 +143,7 @@ function onClear () {
     color: rgba(0, 0, 0, 0.88);
     font-size: 14px;
     line-height: 1.5714285714285714;
+    list-style: none;
     transition: all 0.3s, height 0s;
     resize: vertical;
     position: relative;
@@ -178,10 +166,6 @@ function onClear () {
       outline: 0;
     }
   }
-  .auto-size {
-    max-height: 9000000000000000px;
-    resize: none;
-  }
   textarea:disabled {
     color: rgba(0, 0, 0, 0.25);
   }
@@ -197,48 +181,47 @@ function onClear () {
   textarea:-ms-input-placeholder {
     color: rgba(0, 0, 0, 0.25)
   }
-  .m-suffix {
-    margin-inline-start: 4px;
-    display: flex;
-    flex: none;
-    align-items: center;
-    span {
-      margin-right: 4px;
-    }
-    .m-action {
+  .m-clear {
+    position: absolute;
+    inset-block-start: 8px;
+    inset-inline-end: 8px;
+    z-index: 1;
+    display: inline-block;
+    line-height: 0;
+    .u-clear {
+      display: inline-block;
+      color: rgba(0, 0, 0, 0.25);
+      font-size: 12px;
+      line-height: 1;
+      vertical-align: -1px;
       cursor: pointer;
-      .u-clear {
-        font-size: 12px;
-        display: inline-block;
-        fill: rgba(0, 0, 0, 0.25);
-        text-align: center;
-        line-height: 0;
-        vertical-align: -0.08em;
-        transition: fill 0.3s;
-        &:hover {
-          fill: rgba(0, 0, 0, 0.45);
-        }
+      transition: color 0.3s;
+      transition: fill 0.3s;
+      &:hover {
+        fill: rgba(0, 0, 0, 0.45);
       }
     }
-    .m-count {
-      color: rgba(0, 0, 0, 0.45);
+  }
+  .disabled {
+    color: rgba(0, 0, 0, 0.25);
+    background-color: rgba(0, 0, 0, 0.04);
+    cursor: not-allowed;
+    &:hover {
+      border-color: #d9d9d9;
+    }
+    &:focus-within {
+      border-color: #d9d9d9;
+      box-shadow: none
     }
   }
 }
-.disabled {
-  color: rgba(0, 0, 0, 0.25);
-  background-color: rgba(0, 0, 0, 0.04);
-  cursor: not-allowed;
-  &:hover {
-    border-color: #d9d9d9;
-  }
-  &:focus-within {
-    border-color: #d9d9d9;
-    box-shadow: none
-  }
-  .u-textare {
-    background-color: transparent;
-    cursor: not-allowed;
+.show-count {
+  &::after {
+    color: rgba(0, 0, 0, 0.45);
+    white-space: nowrap;
+    content: attr(data-count);
+    pointer-events: none;
+    float: right;
   }
 }
 </style>
