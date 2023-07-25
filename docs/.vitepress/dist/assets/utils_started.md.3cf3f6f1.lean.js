05bc9ece1d272ad0e7bc7f221f8c98d493038402
themusecatcher <themusecatcher@163.com>
2023-07-25 17:43:42 +0800

    chore: update

---
+++ docs/.vitepress/dist/assets/utils_started.md.3cf3f6f1.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,o as s,U as e}from"./chunks/framework.fa1f3b5c.js";const m=JSON.parse('{"title":"快速上手","description":"","frontmatter":{},"headers":[],"relativePath":"utils/started.md","filePath":"utils/started.md"}'),l={name:"utils/started.md"},n=e("",7),o=[n];function r(p,d,c,i,y,F){return s(),a("div",null,o)}const u=t(l,[["render",r]]);export{m as __pageData,u as default};
