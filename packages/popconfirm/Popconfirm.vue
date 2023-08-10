950eef93558da7d54e4e33c04dcebadac2ef9b25
lhdai <d_muses@163.com>
2023-08-10 15:38:30 +0800

    update

--- packages/popconfirm/Popconfirm.vue
+++ packages/popconfirm/Popconfirm.vue
@@ -6,6 +6,7 @@ interface Props {
   description?: string|Slot // 确认框的内容描述
   content?: string|Slot // 展示的文本
   icon?: string|Slot // 自定义弹出确认框 Icon 图标
+  iconType?: 'success'|'info'|'warn'|'error' // 弹出确认框 Icon 图标类型
   maxWidth?: string|number // 弹出确认框内容最大宽度
   cancelText?: string|Slot // 取消按钮文字
   cancelType?: string // 取消按钮类型
@@ -18,6 +19,7 @@ const props = withDefaults(defineProps<Props>(), {
   description: '',
   content: '',
   icon: '',
+  iconType: 'warn',
   maxWidth: 'auto',
   cancelText: '取消',
   cancelType: 'default',
@@ -92,7 +94,10 @@ function onOk (e: Event) {
         <div class="m-pop-message">
           <span class="m-icon">
             <slot name="icon">
-              <svg focusable="false" class="u-icon" data-icon="exclamation-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path></svg>
+              <svg focusable="false" class="u-icon" v-if="iconType==='info'" width="1em" height="1em" style="fill: #1677FF;" viewBox="64 64 896 896" data-icon="info-circle" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
+              <svg focusable="false" class="u-icon" v-if="iconType==='success'" width="1em" height="1em" style="fill: #52c41a;" viewBox="64 64 896 896" data-icon="check-circle" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 0 1-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"></path></svg>
+              <svg focusable="false" class="u-icon" v-if="iconType==='error'" width="1em" height="1em" style="fill: #ff4d4f;" viewBox="64 64 896 896" data-icon="close-circle" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path></svg>
+              <svg focusable="false" class="u-icon" v-if="iconType==='warn'" width="1em" height="1em" style="fill: #faad14;" viewBox="64 64 896 896" data-icon="exclamation-circle" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
             </slot>
           </span>
           <div class="m-title" :class="{'font-weight': showDesc}">
@@ -169,7 +174,6 @@ function onOk (e: Event) {
           .u-icon {
             display: inline-block;
             line-height: 1;
-            fill: #faad14;
           }
         }
         .m-title {
