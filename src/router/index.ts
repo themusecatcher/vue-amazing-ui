d9364591c275a1d1e7b9339b6cb4b6fb2f372e6a
lhdai <d_muses@163.com>
2023-08-23 16:35:16 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -341,7 +341,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), // 使用history模式，hash模式：createWebHashHistory
   routes, // `routes: routes` 的缩写
   scrollBehavior(to, from, savedPosition) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => { // 延迟滚动
       rafTimeout(() => {
         resolve({ left: 0, top: 0, behavior: 'smooth' })
       }, 300)
