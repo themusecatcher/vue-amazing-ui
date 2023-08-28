10c1a7e17ed77d0b0ba4d0564d554f6af29be455
lhdai <d_muses@163.com>
2023-08-28 17:18:22 +0800

    update

--- packages/dialog/Dialog.vue
+++ packages/dialog/Dialog.vue
@@ -111,7 +111,7 @@ function onConfirm () {
 }
 .v-enter-from, .v-leave-to {
   opacity: 0;
-  transform: scale(.6);
+  transform: scale(.3);
 }
 .flex-hv-center { // 水平垂直居中方法①：弹性布局，随内容增大高度，并自适应水平垂直居中
   display: flex;
