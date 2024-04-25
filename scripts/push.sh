823272f407d8fb3bdfb1e643974cb005c6ad8886
lhdai <d_muses@163.com>
2024-04-25 09:25:59 +0800

    update

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,6 +4,15 @@
 set -e
 
 # 提交修改到github
+
+commitDescInfo=$1
+
 git add .
-git cm -m 'update'
+
+if [ -z ${commitDescInfo} ]; then
+  git cm -m 'update'
+else
+  git cm -m ${commitDescInfo}
+fi
+
 git push
