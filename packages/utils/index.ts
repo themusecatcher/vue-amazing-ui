458f5f052f99ab613566f39a99bc1b824d9a97ae
themusecatcher <d_muses@163.com>
2024-07-20 11:20:04 +0800

    update

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -477,15 +477,19 @@ export function useMediaQuery(mediaQuery = '(max-width: 768px)') {
 /**
  * 组合式函数
  * 使用 ResizeObserver 观察 DOM 元素尺寸变化
- * 
+ *
  * 该函数提供了一种方便的方式来观察一个或多个元素的尺寸变化，并在变化时执行指定的回调函数。
- * 
+ *
  * @param target 要观察的目标，可以是 Ref 对象、Ref 数组、HTMLElement 或 HTMLElement 数组
  * @param callback 当元素尺寸变化时调用的回调函数
  * @param options ResizeObserver 选项，用于定制观察行为
  * @returns 返回一个对象，包含停止和开始观察的方法，使用者可以调用 start 方法开始观察，调用 stop 方法停止观察
  */
-export function useResizeObserver(target: Ref | Ref[] | HTMLElement | HTMLElement[], callback: ResizeObserverCallback, options = {} ) {
+export function useResizeObserver(
+  target: Ref | Ref[] | HTMLElement | HTMLElement[],
+  callback: ResizeObserverCallback,
+  options = {}
+) {
   let observer: ResizeObserver | undefined
   const stopObservation = ref(false)
   const targets = computed(() => {
