ce22dc008f35b7cd45f2e23512b685397e493aab
themusecatcher <d_muses@163.com>
2024-08-18 10:14:42 +0800

    update

--- src/views/guide/NotFound.vue
+++ src/views/guide/NotFound.vue
@@ -5,33 +5,3 @@
     </template>
   </Result>
 </template>
-<style lang="less" scoped>
-.m-not-found {
-  width: 1200px;
-  margin: 0 auto;
-  .m-img {
-    display: inline-block;
-    vertical-align: top;
-  }
-  .m-info {
-    display: inline-block;
-    margin-top: 325px;
-    .u-info {
-      margin-bottom: 30px;
-      font-size: 18px;
-      color: #333;
-    }
-    .u-btn {
-      display: inline-block;
-      text-align: center;
-      border-radius: 3px;
-      color: #fff;
-      font-size: 16px;
-      padding: 0 40px;
-      height: 38px;
-      line-height: 38px;
-      background-color: @themeColor;
-    }
-  }
-}
-</style>
