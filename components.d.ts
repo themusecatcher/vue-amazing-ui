5d3d7c2b117d10fb2ce6416a325f58c4775e2489
themusecatcher <d_muses@163.com>
2024-09-14 23:34:39 +0800

    update

--- components.d.ts
+++ components.d.ts
@@ -7,13 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ACol: typeof import('ant-design-vue/es')['Col']
-    AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
-    AInputNumber: typeof import('ant-design-vue/es')['InputNumber']
-    AMenu: typeof import('ant-design-vue/es')['Menu']
-    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
-    ARow: typeof import('ant-design-vue/es')['Row']
-    ASlider: typeof import('ant-design-vue/es')['Slider']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- docs/guide/components/watermark.md
+++ docs/guide/components/watermark.md
@@ -306,7 +306,7 @@ rotate | 水印绘制时，旋转的角度，单位 `°` | number | -22
 zIndex | 追加的水印元素的 `z-index` | number| 90
 image | 图片源，建议使用 `2` 倍或 `3` 倍图，优先级高于文字 | string | undefined
 content | 水印文字内容 | string &#124; string[] | undefined
-fullscreen | 是否展示全屏 | boolean | false
+fullscreen | 是否启用全屏水印 | boolean | false
 textStyle | 水印文字样式 | [Font](#font-type) | {<br/>&nbsp;&nbsp;color: 'rgba(0, 0, 0, 0.15)',<br/>&nbsp;&nbsp;fontSize: 16,<br/>&nbsp;&nbsp;fontWeight: 'normal',<br/>&nbsp;&nbsp;fontFamily: 'sans-serif',<br/>&nbsp;&nbsp;fontStyle: 'normal' <br/>}
 gap | 水印之间的间距 | [number, number] | [100, 100]
 offset | 水印距离容器左上角的偏移量，默认为 `gap/2` | [number, number] | [50, 50]
