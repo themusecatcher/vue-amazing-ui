2673ac331c519a83253e6b620cdadc5d7ef7ae1a
themusecatcher <themusecatcher@163.com>
2024-09-26 15:05:34 +0800

    chore: update

--- components/backtop/BackTop.vue
+++ components/backtop/BackTop.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, watch, onMounted, onBeforeUnmount, Teleport } from 'vue'
+import { ref, computed, watch, onMounted, onBeforeUnmount } from 'vue'
 import type { Slot } from 'vue'
 import Tooltip from '../tooltip'
 import { useSlotsExist } from '../utils'
