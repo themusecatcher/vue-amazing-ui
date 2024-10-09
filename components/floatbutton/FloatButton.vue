b9b554e8d37df195a49532959ad198b673150743
themusecatcher <themusecatcher@163.com>
2024-10-09 14:34:19 +0800

    chore: update

--- components/floatbutton/FloatButton.vue
+++ components/floatbutton/FloatButton.vue
@@ -104,8 +104,8 @@ function onClick(e: Event) {
       --float-btn-bottom: ${floatBtnBottom}
     `"
     @click="onClick"
-    @mouseenter="menuTrigger === 'hover' ? showMenu = true : null"
-    @mouseleave="menuTrigger === 'hover' ? showMenu = false : null"
+    @mouseenter="menuTrigger === 'hover' ? (showMenu = true) : null"
+    @mouseleave="menuTrigger === 'hover' ? (showMenu = false) : null"
   >
     <Tooltip v-bind="tooltipProps">
       <template v-if="showTooltip" #tooltip>
@@ -124,7 +124,20 @@ function onClick(e: Event) {
             </div>
             <div v-else class="float-btn-content float-btn-close">
               <div class="float-btn-icon">
-                <svg focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true" fill-rule="evenodd" viewBox="64 64 896 896"><path d="M799.86 166.31c.02 0 .04.02.08.06l57.69 57.7c.04.03.05.05.06.08a.12.12 0 010 .06c0 .03-.02.05-.06.09L569.93 512l287.7 287.7c.04.04.05.06.06.09a.12.12 0 010 .07c0 .02-.02.04-.06.08l-57.7 57.69c-.03.04-.05.05-.07.06a.12.12 0 01-.07 0c-.03 0-.05-.02-.09-.06L512 569.93l-287.7 287.7c-.04.04-.06.05-.09.06a.12.12 0 01-.07 0c-.02 0-.04-.02-.08-.06l-57.69-57.7c-.04-.03-.05-.05-.06-.07a.12.12 0 010-.07c0-.03.02-.05.06-.09L454.07 512l-287.7-287.7c-.04-.04-.05-.06-.06-.09a.12.12 0 010-.07c0-.02.02-.04.06-.08l57.7-57.69c.03-.04.05-.05.07-.06a.12.12 0 01.07 0c.03 0 .05.02.09.06L512 454.07l287.7-287.7c.04-.04.06-.05.09-.06a.12.12 0 01.07 0z"></path></svg>
+                <svg
+                  focusable="false"
+                  data-icon="close"
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  fill-rule="evenodd"
+                  viewBox="64 64 896 896"
+                >
+                  <path
+                    d="M799.86 166.31c.02 0 .04.02.08.06l57.69 57.7c.04.03.05.05.06.08a.12.12 0 010 .06c0 .03-.02.05-.06.09L569.93 512l287.7 287.7c.04.04.05.06.06.09a.12.12 0 010 .07c0 .02-.02.04-.06.08l-57.7 57.69c-.03.04-.05.05-.07.06a.12.12 0 01-.07 0c-.03 0-.05-.02-.09-.06L512 569.93l-287.7 287.7c-.04.04-.06.05-.09.06a.12.12 0 01-.07 0c-.02 0-.04-.02-.08-.06l-57.69-57.7c-.04-.03-.05-.05-.06-.07a.12.12 0 010-.07c0-.03.02-.05.06-.09L454.07 512l-287.7-287.7c-.04-.04-.05-.06-.06-.09a.12.12 0 010-.07c0-.02.02-.04.06-.08l57.7-57.69c.03-.04.05-.05.07-.06a.12.12 0 01.07 0c.03 0 .05.02.09.06L512 454.07l287.7-287.7c.04-.04.06-.05.09-.06a.12.12 0 01.07 0z"
+                  ></path>
+                </svg>
               </div>
             </div>
           </Transition>
@@ -141,7 +154,9 @@ function onClick(e: Event) {
 <style lang="less" scoped>
 .fade-enter-active,
 .fade-leave-active {
-  transition: transform 0.3s cubic-bezier(0.4, 0, 0.2, 1), opacity 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  transition:
+    transform 0.3s cubic-bezier(0.4, 0, 0.2, 1),
+    opacity 0.3s cubic-bezier(0.4, 0, 0.2, 1);
 }
 .fade-enter-from,
 .fade-leave-to {
