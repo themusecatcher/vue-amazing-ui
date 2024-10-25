31e107c939c5e020ca98e887cd306297487fa801
themusecatcher <themusecatcher@163.com>
2024-10-25 10:16:57 +0800

    chore: update

--- src/layouts/GlobalLayout.vue
+++ src/layouts/GlobalLayout.vue
@@ -46,11 +46,11 @@ const countdown = computed(() => {
 const routerViewRef = ref()
 </script>
 <template>
-  <Switch v-model="showDuty" class="u-duty-switch" />
+  <Switch v-model="showDuty" class="switch-duty" />
   <Transition>
     <Countdown
       v-show="showDuty"
-      class="u-duty-countdown"
+      class="countdown-duty"
       title="Off Duty"
       :title-style="{ fontWeight: 600 }"
       :value="countdown"
@@ -62,20 +62,20 @@ const routerViewRef = ref()
   <Row style="width: 100%">
     <Col :xs="5" :xl="4" style="position: relative">
       <Switch
-        class="u-theme-switch"
+        class="switch-theme"
         v-model="themeDark"
-        @change="toggleDark"
         ripple-color="#faad14"
         :circle-style="{ background: themeDark ? '#001529' : '#fff' }"
+        @change="toggleDark"
       >
         <template #node>
           <svg
             v-if="themeDark"
+            class="u-dark-svg"
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
             focusable="false"
             viewBox="0 0 24 24"
-            class="u-dark-svg"
           >
             <path
               d="M12.1,22c-0.3,0-0.6,0-0.9,0c-5.5-0.5-9.5-5.4-9-10.9c0.4-4.8,4.2-8.6,9-9c0.4,0,0.8,0.2,1,0.5c0.2,0.3,0.2,0.8-0.1,1.1c-2,2.7-1.4,6.4,1.3,8.4c2.1,1.6,5,1.6,7.1,0c0.3-0.2,0.7-0.3,1.1-0.1c0.3,0.2,0.5,0.6,0.5,1c-0.2,2.7-1.5,5.1-3.6,6.8C16.6,21.2,14.4,22,12.1,22zM9.3,4.4c-2.9,1-5,3.6-5.2,6.8c-0.4,4.4,2.8,8.3,7.2,8.7c2.1,0.2,4.2-0.4,5.8-1.8c1.1-0.9,1.9-2.1,2.4-3.4c-2.5,0.9-5.3,0.5-7.5-1.1C9.2,11.4,8.1,7.7,9.3,4.4z"
@@ -148,19 +148,19 @@ const routerViewRef = ref()
 .v-leave-to {
   opacity: 0;
 }
-.u-duty-switch {
+.switch-duty {
   position: fixed;
   top: 36px;
   right: 36px;
   z-index: 3;
 }
-.u-duty-countdown {
+.countdown-duty {
   position: fixed;
   top: 36px;
   right: 36px;
   z-index: 2;
 }
-.u-theme-switch {
+.switch-theme {
   position: absolute;
   top: 13px;
   right: 13px;
