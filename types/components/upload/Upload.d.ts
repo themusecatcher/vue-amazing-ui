a07fe539e7629067c109315553d6c3d42c7a2cf2
lhdai <d_muses@163.com>
2024-12-03 10:20:43 +0800

    update

--- types/components/upload/Upload.d.ts
+++ types/components/upload/Upload.d.ts
@@ -36,8 +36,140 @@ declare function __VLS_template(): {
     };
     refs: {
         uploadInputRef: HTMLInputElement;
-        imageRef: any;
-        messageRef: any;
+        imageRef: ({
+            $: import('vue').ComponentInternalInstance;
+            $data: {};
+            $props: {
+                readonly src?: (string | import('../image').Image[]) | undefined;
+                readonly name?: string | undefined;
+                readonly width?: (string | number | (string | number)[]) | undefined;
+                readonly height?: (string | number | (string | number)[]) | undefined;
+                readonly bordered?: boolean | undefined;
+                readonly fit?: ("contain" | "fill" | "cover" | "none" | "scale-down") | undefined;
+                readonly preview?: string | undefined;
+                readonly spaceProps?: object | undefined;
+                readonly spinProps?: object | undefined;
+                readonly zoomRatio?: number | undefined;
+                readonly minZoomScale?: number | undefined;
+                readonly maxZoomScale?: number | undefined;
+                readonly resetOnDbclick?: boolean | undefined;
+                readonly loop?: boolean | undefined;
+                readonly album?: boolean | undefined;
+            } & import('vue').VNodeProps & import('vue').AllowedComponentProps & import('vue').ComponentCustomProps;
+            $attrs: {
+                [x: string]: unknown;
+            };
+            $refs: {
+                [x: string]: unknown;
+            };
+            $slots: Readonly<{
+                [name: string]: import('vue').Slot<any> | undefined;
+            }>;
+            $root: import('vue').ComponentPublicInstance | null;
+            $parent: import('vue').ComponentPublicInstance | null;
+            $host: Element | null;
+            $emit: (event: string, ...args: any[]) => void;
+            $el: any;
+            $options: import('vue').ComponentOptionsBase<Readonly<import('../image').Props> & Readonly<{}>, {
+                preview: (index: number) => Promise<void>;
+            }, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, {
+                bordered: boolean;
+                name: string;
+                src: string | import('../image').Image[];
+                width: string | number | (string | number)[];
+                height: string | number | (string | number)[];
+                spinProps: object;
+                fit: "contain" | "fill" | "cover" | "none" | "scale-down";
+                preview: string;
+                spaceProps: object;
+                zoomRatio: number;
+                minZoomScale: number;
+                maxZoomScale: number;
+                resetOnDbclick: boolean;
+                loop: boolean;
+                album: boolean;
+            }, {}, string, {}, import('vue').GlobalComponents, import('vue').GlobalDirectives, string, import('vue').ComponentProvideOptions> & {
+                beforeCreate?: (() => void) | (() => void)[];
+                created?: (() => void) | (() => void)[];
+                beforeMount?: (() => void) | (() => void)[];
+                mounted?: (() => void) | (() => void)[];
+                beforeUpdate?: (() => void) | (() => void)[];
+                updated?: (() => void) | (() => void)[];
+                activated?: (() => void) | (() => void)[];
+                deactivated?: (() => void) | (() => void)[];
+                beforeDestroy?: (() => void) | (() => void)[];
+                beforeUnmount?: (() => void) | (() => void)[];
+                destroyed?: (() => void) | (() => void)[];
+                unmounted?: (() => void) | (() => void)[];
+                renderTracked?: ((e: import('vue').DebuggerEvent) => void) | ((e: import('vue').DebuggerEvent) => void)[];
+                renderTriggered?: ((e: import('vue').DebuggerEvent) => void) | ((e: import('vue').DebuggerEvent) => void)[];
+                errorCaptured?: ((err: unknown, instance: import('vue').ComponentPublicInstance | null, info: string) => boolean | void) | ((err: unknown, instance: import('vue').ComponentPublicInstance | null, info: string) => boolean | void)[];
+            };
+            $forceUpdate: () => void;
+            $nextTick: typeof import('vue').nextTick;
+            $watch<T extends string | ((...args: any) => any)>(source: T, cb: T extends (...args: any) => infer R ? (...args: [R, R, import('@vue/reactivity').OnCleanup]) => any : (...args: [any, any, import('@vue/reactivity').OnCleanup]) => any, options?: import('vue').WatchOptions): import('vue').WatchStopHandle;
+        } & Readonly<{
+            bordered: boolean;
+            name: string;
+            src: string | import('../image').Image[];
+            width: string | number | (string | number)[];
+            height: string | number | (string | number)[];
+            spinProps: object;
+            fit: "contain" | "fill" | "cover" | "none" | "scale-down";
+            preview: string;
+            spaceProps: object;
+            zoomRatio: number;
+            minZoomScale: number;
+            maxZoomScale: number;
+            resetOnDbclick: boolean;
+            loop: boolean;
+            album: boolean;
+        }> & Omit<Readonly<import('../image').Props> & Readonly<{}>, "bordered" | "name" | "src" | "width" | "height" | "spinProps" | "fit" | "preview" | "spaceProps" | "zoomRatio" | "minZoomScale" | "maxZoomScale" | "resetOnDbclick" | "loop" | "album"> & import('vue').ShallowUnwrapRef<{
+            preview: (index: number) => Promise<void>;
+        }> & {} & import('vue').ComponentCustomProperties & {} & {
+            $slots: {
+                preview?(_: {}): any;
+            };
+        }) | null;
+        messageRef: import('vue').CreateComponentPublicInstanceWithMixins<Readonly<import('../message').Props> & Readonly<{
+            onClick?: ((...args: any[]) => any) | undefined;
+            onClose?: ((...args: any[]) => any) | undefined;
+        }>, {
+            open: (message: string | import('../message').Message) => void;
+            info: (message: string | import('../message').Message) => void;
+            success: (message: string | import('../message').Message) => void;
+            error: (message: string | import('../message').Message) => void;
+            warning: (message: string | import('../message').Message) => void;
+            loading: (message: string | import('../message').Message) => void;
+        }, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
+            click: (...args: any[]) => void;
+            close: (...args: any[]) => void;
+        }, import('vue').PublicProps, {
+            duration: number;
+            top: string | number;
+            content: string;
+        }, false, {}, {}, import('vue').GlobalComponents, import('vue').GlobalDirectives, string, {}, any, import('vue').ComponentProvideOptions, {
+            P: {};
+            B: {};
+            D: {};
+            C: {};
+            M: {};
+            Defaults: {};
+        }, Readonly<import('../message').Props> & Readonly<{
+            onClick?: ((...args: any[]) => any) | undefined;
+            onClose?: ((...args: any[]) => any) | undefined;
+        }>, {
+            open: (message: string | import('../message').Message) => void;
+            info: (message: string | import('../message').Message) => void;
+            success: (message: string | import('../message').Message) => void;
+            error: (message: string | import('../message').Message) => void;
+            warning: (message: string | import('../message').Message) => void;
+            loading: (message: string | import('../message').Message) => void;
+        }, {}, {}, {}, {
+            duration: number;
+            top: string | number;
+            content: string;
+        }> | null;
     };
     attrs: Partial<{}>;
 };
