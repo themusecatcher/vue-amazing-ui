e48e5b0ab3ee2da7637104963442989a7fbbd925
lhdai <d_muses@163.com>
2024-12-09 09:50:13 +0800

    update

--- components/textscroll/TextScroll.vue
+++ components/textscroll/TextScroll.vue
@@ -1,67 +1,58 @@
 <script setup lang="ts">
-import { ref, computed, watch, watchEffect } from 'vue'
+import { ref, computed, watch, nextTick } from 'vue'
 import type { CSSProperties } from 'vue'
-import { rafTimeout, cancelRaf, useResizeObserver } from '../utils'
-export interface Text {
+import { rafTimeout, cancelRaf, useResizeObserver } from 'components/utils'
+export interface Item {
   title: string // 文字标题
   href?: string // 跳转链接
   target?: '_self' | '_blank' // 跳转链接打开方式，href 存在时生效
 }
 export interface Props {
-  scrollText?: Text[] | Text // 滚动文字数组，single 为 true 时，类型为 Text；多条文字滚动时，数组长度必须大于等于 amount 才能滚动
+  items?: Item[] | Item // 滚动文字数组，single 为 true 时，类型为 Item；多条文字滚动时，数组长度必须大于等于 amount 才能滚动
   single?: boolean // 是否启用单条文字滚动效果，只支持水平文字滚动，为 true 时，amount 自动设为 1
   width?: number | string // 滚动区域宽度，单位 px
   height?: number // 滚动区域高度，单位 px
-  boardStyle?: CSSProperties // 滚动区域样式，优先级低于 width、height
-  textStyle?: CSSProperties // 滚动文字样式
+  itemStyle?: CSSProperties // 滚动文字样式
   hrefHoverColor?: string // 链接文字鼠标悬浮颜色；仅当 href 存在时生效
   amount?: number // 滚动区域展示条数，水平滚动时生效
   gap?: number // 水平滚动文字各列间距或垂直滚动文字两边的边距，单位 px
-  interval?: number // 水平滚动动画执行时间间隔，单位 ms，水平滚动时生效
-  step?: number // 水平滚动动画每次执行时移动距离，单位 px，水平滚动时生效，与 interval 配合控制滚动速度
+  speed?: number // 水平滚动时移动的速度，单位是像素每秒，水平滚动时生效
   vertical?: boolean // 是否垂直滚动
   verticalInterval?: number // 垂直文字滚动时间间隔，单位 ms，垂直滚动时生效
+  pauseOnMouseEnter?: boolean // 鼠标移入是否暂停滚动
 }
 const props = withDefaults(defineProps<Props>(), {
-  scrollText: () => [],
+  items: () => [],
   single: false,
   width: '100%',
   height: 50,
-  boardStyle: () => ({}),
-  textStyle: () => ({}),
+  itemStyle: () => ({}),
   hrefHoverColor: '#1677ff',
   amount: 4,
   gap: 20,
-  interval: 10,
-  step: 1,
+  speed: 48,
   vertical: false,
-  verticalInterval: 3000
+  verticalInterval: 3000,
+  pauseOnMouseEnter: true
 })
 const horizontalRef = ref() // 水平滚动 DOM 引用
+const horizontalWrapWidth = ref<number>(0) // 水平滚动容器宽度
 const verticalRef = ref() // 垂直滚动 DOM 引用
-const left = ref(0) // 水平滚动偏移距离
-const distance = ref(0) // 每条滚动文字移动距离
+const groupRef = ref() // 水平滚动内容 DOM 引用
+const groupWidth = ref<number>(0) // 水平滚动内容宽度
+const playState = ref<'paused' | 'running'>('paused') // 水平滚动动画执行状态
+const reset = ref(true) // 重置水平滚动动画状态
 const activeIndex = ref(0) // 垂直滚动当前索引
-const horizontalMoveRaf = ref()
-const verticalMoveRaf = ref()
+const verticalMoveRaf = ref() // 垂直滚动定时器引用标识
 const origin = ref(true) // 垂直滚动初始状态
 const emit = defineEmits(['click'])
-const textData = ref<Text[]>([])
-const textAmount = computed(() => {
-  return textData.value.length
+const scrollItems = ref<Item[]>([])
+const itemsAmount = computed(() => {
+  return scrollItems.value.length
 })
-const totalWidth = computed(() => {
-  // 文字滚动区域总宽度
-  if (typeof props.width === 'number') {
-    return `${props.width}px`
-  } else {
-    return props.width
-  }
-})
-const sliderBoardStyle = computed(() => {
+const scrollBoardStyle = computed(() => {
   return {
-    ...props.boardStyle,
-    width: totalWidth.value,
+    width: typeof props.width === 'number' ? `${props.width}px` : props.width,
     height: `${props.height}px`
   }
 })
@@ -72,17 +63,34 @@ const displayAmount = computed(() => {
     return props.amount
   }
 })
+const itemWidth = computed(() => {
+  // 水平滚动单条文字宽度
+  return parseFloat((horizontalWrapWidth.value / displayAmount.value).toFixed(2))
+})
+const duration = computed(() => {
+  return groupWidth.value / props.speed
+})
 watch(
-  () => [
-    textData.value,
-    props.width,
-    props.amount,
-    props.gap,
-    props.step,
-    props.interval,
-    props.vertical,
-    props.verticalInterval
-  ],
+  () => props.items,
+  () => {
+    if (props.single) {
+      scrollItems.value = [props.items] as Item[]
+    } else {
+      scrollItems.value = [...(props.items as Item[])]
+    }
+  },
+  {
+    immediate: true
+  }
+)
+watch(scrollItems, () => {
+  resetMove()
+  nextTick(() => {
+    startMove()
+  })
+})
+watch(
+  () => [props.vertical, props.verticalInterval],
   () => {
     initScroll()
   },
@@ -91,149 +99,170 @@ watch(
     flush: 'post'
   }
 )
-watchEffect(() => {
-  initScrollText()
-})
 useResizeObserver([horizontalRef, verticalRef], () => {
   initScroll()
 })
-function initScrollText() {
-  left.value = 0
-  activeIndex.value = 0
-  if (props.single) {
-    textData.value = [props.scrollText, props.scrollText] as Text[]
-  } else {
-    const text = props.scrollText as Text[]
-    if (text.length === props.amount) {
-      textData.value = [...text, ...text]
-    } else {
-      textData.value = [...text]
-    }
-  }
-}
 function initScroll() {
   if (!props.vertical) {
-    distance.value = getDistance() // 获取每列文字宽度
+    getScrollSize()
   } else {
     origin.value = true
   }
-  horizontalMoveRaf.value && cancelRaf(horizontalMoveRaf.value)
-  verticalMoveRaf.value && cancelRaf(verticalMoveRaf.value)
   startMove() // 开始滚动
 }
-function getDistance(): number {
-  return parseFloat((horizontalRef.value.offsetWidth / displayAmount.value).toFixed(2))
+// 获取水平滚动容器宽度；水平滚动内容宽度
+function getScrollSize() {
+  horizontalWrapWidth.value = horizontalRef.value.offsetWidth
+  groupWidth.value = groupRef.value.offsetWidth
 }
+// 重置水平滚动状态
+function resetScrollState() {
+  playState.value = 'paused'
+  nextTick(() => {
+    playState.value = 'running'
+  })
+}
+// 当 CSS Animation 运动到最后一帧时触发
+function onAnimationIteration() {
+  resetScrollState()
+}
+// 滚动开始
 function startMove() {
   if (props.vertical) {
-    if (textAmount.value > 1) {
+    if (itemsAmount.value > 1) {
       verticalMoveRaf.value && cancelRaf(verticalMoveRaf.value)
       verticalMove() // 垂直滚动
     }
   } else {
-    if (textAmount.value > displayAmount.value) {
+    if (itemsAmount.value >= displayAmount.value) {
       // 超过 amount 条开始滚动
-      horizontalMoveRaf.value && cancelRaf(horizontalMoveRaf.value)
-      horizonMove() // 水平滚动
+      reset.value = false
+      playState.value = 'running' // 水平滚动
     }
   }
 }
+// 滚动暂停
 function stopMove() {
-  // 暂停动画
   if (props.vertical) {
     verticalMoveRaf.value && cancelRaf(verticalMoveRaf.value)
   } else {
-    horizontalMoveRaf.value && cancelRaf(horizontalMoveRaf.value)
+    playState.value = 'paused'
   }
 }
-function horizonMove() {
-  horizontalMoveRaf.value = rafTimeout(
-    () => {
-      if (left.value >= distance.value) {
-        textData.value.push(textData.value.shift() as Text) // 将第一条数据放到最后
-        left.value = 0
-      } else {
-        left.value += props.step // 每次移动 step px
-      }
-    },
-    props.interval,
-    true
-  )
+// 滚动重置
+function resetMove() {
+  if (props.vertical) {
+    activeIndex.value = 0
+  } else {
+    playState.value = 'paused'
+    reset.value = true
+  }
 }
+
 function verticalMove() {
   verticalMoveRaf.value = rafTimeout(
     () => {
       if (origin.value) {
         origin.value = false
       }
-      activeIndex.value = (activeIndex.value + 1) % textAmount.value
+      activeIndex.value = (activeIndex.value + 1) % itemsAmount.value
       verticalMove()
     },
     origin.value ? props.verticalInterval : props.verticalInterval + 1000
   )
 }
-function onClick(text: Text) {
-  emit('click', text)
+function onClick(item: Item) {
+  emit('click', item)
 }
 defineExpose({
   start: startMove,
   stop: stopMove,
-  reset: initScrollText
+  reset: resetMove
 })
 </script>
 <template>
   <div
     v-if="!vertical"
     ref="horizontalRef"
-    class="m-slider-horizontal"
+    class="m-scroll-horizontal"
     :style="[
-      sliderBoardStyle,
-      `--shadow-color: #d3d3d3; --bg-color: #fff; --href-hover-color: ${hrefHoverColor}; --text-gap: ${gap}px;`
+      scrollBoardStyle,
+      `
+        --scroll-shadow-color: #d3d3d3;
+        --scroll-bg-color: #fff;
+        --scroll-href-hover-color: ${hrefHoverColor};
+        --scroll-item-gap: ${gap}px;
+        --scroll-play-state: ${playState};
+        --scroll-duration: ${duration}s;
+        --scroll-delay: 0s;
+        --scroll-iteration-count: infinite;
+      `
     ]"
+    @mouseenter="pauseOnMouseEnter ? stopMove() : () => false"
+    @mouseleave="pauseOnMouseEnter ? startMove() : () => false"
   >
-    <div class="m-scroll-view" :style="`will-change: transform; transform: translateX(${-left}px);`">
+    <div
+      ref="groupRef"
+      class="scroll-items-group"
+      :class="{ 'scroll-items-reset': reset }"
+      @animationiteration="onAnimationIteration"
+    >
       <component
-        :is="text.href ? 'a' : 'div'"
-        class="slide-text"
-        :class="{ 'href-text': text.href }"
-        :style="[textStyle, `width: ${distance}px;`]"
-        v-for="(text, index) in <Text[]>textData"
+        :is="item.href ? 'a' : 'div'"
+        class="scroll-item"
+        :class="{ 'href-item': item.href }"
+        :style="[itemStyle, `width: ${itemWidth}px;`]"
+        v-for="(item, index) in <Item[]>scrollItems"
         :key="index"
-        :title="text.title"
-        :href="text.href"
-        :target="text.target"
-        @mouseenter="stopMove"
-        @mouseleave="startMove"
-        @click="onClick(text)"
+        :title="item.title"
+        :href="item.href"
+        :target="item.target"
+        @click="onClick(item)"
       >
-        {{ text.title }}
+        {{ item.title }}
+      </component>
+    </div>
+    <div class="scroll-items-group" :class="{ 'scroll-items-reset': reset }">
+      <component
+        :is="item.href ? 'a' : 'div'"
+        class="scroll-item"
+        :class="{ 'href-item': item.href }"
+        :style="[itemStyle, `width: ${itemWidth}px;`]"
+        v-for="(item, index) in scrollItems"
+        :key="index"
+        :title="item.title"
+        :href="item.href"
+        :target="item.target"
+        @click="onClick(item)"
+      >
+        {{ item.title }}
       </component>
     </div>
   </div>
   <div
     v-else
     ref="verticalRef"
-    class="m-slider-vertical"
+    class="m-scroll-vertical"
     :style="[
-      sliderBoardStyle,
-      `--shadow-color: #d3d3d3; --bg-color: #fff; --href-hover-color: ${hrefHoverColor}; --enter-move: ${height}px; --leave-move: ${-height}px; --tex-gap: ${gap}px;`
+      scrollBoardStyle,
+      `--scroll-shadow-color: #d3d3d3; --scroll-bg-color: #fff; --scroll-href-hover-color: ${hrefHoverColor}; --enter-move: ${height}px; --leave-move: ${-height}px; --tex-gap: ${gap}px;`
     ]"
   >
     <TransitionGroup name="slide">
-      <div class="m-scroll-view" v-for="(text, index) in <Text[]>textData" :key="index" v-show="activeIndex === index">
+      <div class="scroll-item-wrap" v-for="(item, index) in scrollItems" :key="index" v-show="activeIndex === index">
         <component
-          :is="text.href ? 'a' : 'div'"
-          class="slide-text"
-          :class="{ 'href-text': text.href }"
-          :style="textStyle"
-          :title="text.title"
-          :href="text.href"
-          :target="text.target"
-          @mouseenter="stopMove"
-          @mouseleave="startMove"
-          @click="onClick(text)"
+          :is="item.href ? 'a' : 'div'"
+          class="scroll-item"
+          :class="{ 'href-item': item.href }"
+          :style="itemStyle"
+          :title="item.title"
+          :href="item.href"
+          :target="item.target"
+          @mouseenter="pauseOnMouseEnter ? stopMove() : () => false"
+          @mouseleave="pauseOnMouseEnter ? startMove() : () => false"
+          @click="onClick(item)"
         >
-          {{ text.title }}
+          {{ item.title }}
         </component>
       </div>
     </TransitionGroup>
@@ -241,17 +270,31 @@ defineExpose({
 </template>
 <style lang="less" scoped>
 // 水平滚动
-.m-slider-horizontal {
+.m-scroll-horizontal {
   overflow: hidden;
-  box-shadow: 0px 0px 5px var(--shadow-color);
+  display: flex;
+  box-shadow: 0px 0px 5px var(--scroll-shadow-color);
   border-radius: 6px;
-  background-color: var(--bg-color);
-  .m-scroll-view {
-    height: 100%;
-    display: inline-flex;
+  background-color: var(--scroll-bg-color);
+  .scroll-items-group {
+    min-width: 100%;
+    flex: 0 0 auto;
+    z-index: 1;
+    display: flex;
+    flex-direction: row;
     align-items: center;
-    .slide-text {
-      padding-left: var(--text-gap);
+    animation: horizontalScroll var(--scroll-duration) linear var(--scroll-delay) var(--scroll-iteration-count);
+    animation-play-state: var(--scroll-play-state);
+    @keyframes horizontalScroll {
+      0% {
+        transform: translateX(0);
+      }
+      100% {
+        transform: translateX(-100%);
+      }
+    }
+    .scroll-item {
+      padding-left: var(--scroll-item-gap);
       font-size: 16px;
       font-weight: 400;
       color: rgba(0, 0, 0, 0.88);
@@ -260,13 +303,16 @@ defineExpose({
       text-overflow: ellipsis;
       white-space: nowrap;
     }
-    .href-text {
+    .href-item {
       cursor: pointer;
       transition: color 0.3s;
       &:hover {
-        color: var(--href-hover-color) !important;
+        color: var(--scroll-href-hover-color) !important;
       }
     }
+  }
+  .scroll-items-reset {
+    animation: none;
   }
 }
 // 垂直滚动
@@ -282,13 +328,13 @@ defineExpose({
   transform: translateY(var(--leave-move)) scale(0.5);
   opacity: 0;
 }
-.m-slider-vertical {
+.m-scroll-vertical {
   position: relative;
   overflow: hidden;
-  box-shadow: 0px 0px 5px var(--shadow-color);
+  box-shadow: 0px 0px 5px var(--scroll-shadow-color);
   border-radius: 6px;
-  background-color: var(--bg-color);
-  .m-scroll-view {
+  background-color: var(--scroll-bg-color);
+  .scroll-item-wrap {
     position: absolute;
     left: 0;
     right: 0;
@@ -297,7 +343,7 @@ defineExpose({
     align-items: center;
     justify-content: center;
     padding: 0 var(--tex-gap);
-    .slide-text {
+    .scroll-item {
       font-size: 16px;
       font-weight: 400;
       color: rgba(0, 0, 0, 0.88);
@@ -306,11 +352,11 @@ defineExpose({
       white-space: nowrap;
       text-overflow: ellipsis;
     }
-    .href-text {
+    .href-item {
       cursor: pointer;
       transition: color 0.3s;
       &:hover {
-        color: var(--href-hover-color) !important;
+        color: var(--scroll-href-hover-color) !important;
       }
     }
   }
