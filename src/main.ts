edd64174e728ff50806923c906d37d0abdec7cd3
lhdai <d_muses@163.com>
2024-12-16 11:08:24 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,26 @@ import App from './App.vue'
 import router from './router'
 import 'less/global.less'
 
-import VueAmazingUI from 'components/index'
+// import VueAmazingUI from 'components/index'
 // import VueAmazingUI from '../dist/vue-amazing-ui'
 // import '../dist/vue-amazing-ui.css'
 // import VueAmazingUI from '../es/index'
-// import '../es/vue-amazing-ui.css'
 // import VueAmazingUI from '../lib/index'
-// import '../lib/vue-amazing-ui.css'
 // import '../dist/vue-amazing-ui.css'
 // import VueAmazingUI from 'vue-amazing-ui'
 // import 'vue-amazing-ui/css'
+import { Row, Col, Button, Scrollbar, Radio, Switch, Space, Tooltip, Watermark } from '../es/index'
+import '../es/grid/row/Row.css'
+import '../es/grid/col/Col.css'
+import '../es/button/Button.css'
+import '../es/scrollbar/Scrollbar.css'
+import '../es/radio/Radio.css'
+import '../es/switch/Switch.css'
+import '../es/space/Space.css'
+import '../es/tooltip/Tooltip.css'
 
 const app = createApp(App)
 // window.rafTimeout = rafTimeout // 挂载到window上，全局可用，无需引入
-app.use(router).use(VueAmazingUI).mount('#app')
+app.use(router)
+app.use(Row).use(Col).use(Button).use(Scrollbar).use(Radio).use(Switch).use(Space).use(Tooltip).use(Watermark)
+app.mount('#app')
