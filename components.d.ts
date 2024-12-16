471f2b0017ff48cf058c0a1c02fd344d9c558c9a
themusecatcher <themusecatcher@163.com>
2024-12-16 15:33:15 +0800

    chore: update

--- components.d.ts
+++ components.d.ts
@@ -7,15 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
-    AMenu: typeof import('ant-design-vue/es')['Menu']
-    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
-    Col: typeof import('vue-amazing-ui')['Col']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Row: typeof import('vue-amazing-ui')['Row']
-    Scrollbar: typeof import('vue-amazing-ui')['Scrollbar']
-    Switch: typeof import('vue-amazing-ui')['Switch']
-    Watermark: typeof import('vue-amazing-ui')['Watermark']
   }
 }
