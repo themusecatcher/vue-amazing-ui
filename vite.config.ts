035ab6ef5516b2efe293c048c960ba53b90a3d15
lhdai <d_muses@163.com>
2024-12-16 14:40:40 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import type { BuildEnvironmentOptions } from 'vite'
 import vue from '@vitejs/plugin-vue'
- // 自动生成类型文件
+// 自动生成类型文件
 import dts from 'vite-plugin-dts'
 // import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -28,8 +28,8 @@ const buildDistOptions = {
     // __dirname 的值是 vite.config.ts 文件所在目录
     entry: resolve(__dirname, 'components', 'index.ts'),  // entry 是必需的，因为库不能使用HTML作为入口。
     name: 'VueAmazingUI', // 暴露的全局变量
-    fileName: 'vue-amazing-ui' // 输出的包文件名，默认是 package.json 的 name 选项；也可以定义为以 format 和 entryName 为参数的函数，并返回文件名
-    // cssFileName: 'vue-amazing-ui' // 指定 CSS 输出文件的名称，默认为 package.json 中的 name
+    fileName: 'index.min', // 输出的包文件名，默认是 package.json 的 name 选项；也可以定义为以 format 和 entryName 为参数的函数，并返回文件名
+    cssFileName: 'style' // 指定 CSS 输出文件的名称，默认为 package.json 中的 name
   },
   rollupOptions: { // 自定义底层的Rollup打包配置
     plugins: [
@@ -204,6 +204,97 @@ const buildESAndLibOptions = {
   // chunkSizeWarningLimit: 500, // 默认 500，规定触发警告的 chunk 大小，单位kbs
   // sourcemap: false // boolean | 'inline' | 'hidden'，构建后是否生成 source map 文件。默认 false
 }
+function VueAmazingUIResolver() {
+  return {
+    type: 'component' as const,
+    resolve: (componentName: string) => {
+      // 定义组件库中的组件名称映射
+      const componentsMap = {
+        Alert: 'alert',
+        Avatar: 'avatar',
+        BackTop: 'backtop',
+        Badge: 'badge',
+        Breadcrumb: 'breadcrumb',
+        Button: 'button',
+        Card: 'card',
+        Carousel: 'carousel',
+        Cascader: 'cascader',
+        Checkbox: 'checkbox',
+        Collapse: 'collapse',
+        Countdown: 'countdown',
+        DatePicker: 'datepicker',
+        Descriptions: 'descriptions/descriptions',
+        DescriptionsItem: 'descriptions/descriptionsitem',
+        Dialog: 'dialog',
+        Divider: 'divider',
+        Drawer: 'drawer',
+        Ellipsis: 'ellipsis',
+        Empty: 'empty',
+        Flex: 'flex',
+        FloatButton: 'floatbutton',
+        GradientText: 'gradienttext',
+        Row: 'grid/row',
+        Col: 'grid/col',
+        Image: 'image',
+        Input: 'input',
+        InputNumber: 'inputnumber',
+        InputSearch: 'inputsearch',
+        Layout: 'layout',
+        LayoutHeader: 'layout/layoutheader',
+        LayoutSider: 'layout/layoutsider',
+        LayoutContent: 'layout/layoutcontent',
+        LayoutFooter: 'layout/layoutfooter',
+        List: 'list/list',
+        ListItem: 'list/listitem',
+        LoadingBar: 'loadingbar',
+        Message: 'message',
+        Modal: 'modal',
+        Notification: 'notification',
+        NumberAnimation: 'numberanimation',
+        Pagination: 'pagination',
+        Popconfirm: 'popconfirm',
+        Popover: 'popover',
+        Progress: 'progress',
+        QRCode: 'qrcode',
+        Radio: 'radio',
+        Rate: 'rate',
+        Result: 'result',
+        Scrollbar: 'scrollbar',
+        Segmented: 'segmented',
+        Select: 'select',
+        Skeleton: 'skeleton',
+        Slider: 'slider',
+        Space: 'space',
+        Spin: 'spin',
+        Statistic: 'statistic',
+        Steps: 'steps',
+        Swiper: 'swiper',
+        Switch: 'switch',
+        Table: 'table',
+        Tabs: 'tabs',
+        Tag: 'tag',
+        Textarea: 'textarea',
+        TextScroll: 'textscroll',
+        Timeline: 'timeline',
+        TimePicker: 'timepicker',
+        Tooltip: 'tooltip',
+        Upload: 'upload',
+        Video: 'video',
+        Waterfall: 'waterfall',
+        Watermark: 'watermark'
+      }
+      console.log('componentName', componentName)
+      // where `componentName` is always CapitalCase
+      if (componentName in componentsMap) {
+        return {
+          name: componentName, // 组件名
+          from: 'vue-amazing-ui', // 组件库名称
+          sideEffects: `vue-amazing-ui/${componentsMap[componentName as keyof typeof componentsMap]}/${componentName}.css`, // 组件样式文件
+        }
+      }
+    }
+  }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -248,6 +339,16 @@ export default defineConfig({
         }
       }
     }),
+    Components({
+      resolvers: [ // ant design, naive ui 自动按需引入
+        AntDesignVueResolver({
+          importStyle: false // css in js
+        }),
+        NaiveUiResolver(),
+        // auto import components from VueAmazingUI
+        // VueAmazingUIResolver()
+      ]
+    })
     // AutoImport({ // 自动引入所需 apis
     //   dts: 'src/auto-imports.d.ts',
     //   imports: ['vue', 'vue-router'],
@@ -260,15 +361,7 @@ export default defineConfig({
     // visualizer({ // 生成的分析图文件名，默认stats.html
     //   // file: 'stats.html',
     //   open: true // 打包后自动打开分析图
-    // }),
-    Components({
-      resolvers: [ // ant design, naive ui 自动按需引入
-        AntDesignVueResolver({
-          importStyle: false // css in js
-        }),
-        NaiveUiResolver()
-      ]
-    })
+    // })
   ],
   // 构建为库
   build: (dir === 'dist' ? buildDistOptions : buildESAndLibOptions) as BuildEnvironmentOptions,
