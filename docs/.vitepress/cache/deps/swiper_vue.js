f5cc6cc61e1bbef4f80467bc79cbac2cfec3dbfe
themusecatcher <themusecatcher@163.com>
2024-12-28 10:38:28 +0800

    chore: update

--- docs/.vitepress/cache/deps/swiper_vue.js
+++ docs/.vitepress/cache/deps/swiper_vue.js
@@ -1,17 +1,4 @@
 import {
-  computed,
-  h,
-  inject,
-  nextTick,
-  onBeforeUnmount,
-  onBeforeUpdate,
-  onMounted,
-  onUpdated,
-  provide,
-  ref,
-  watch
-} from "./chunk-X54IR6VG.js";
-import {
   animateCSSModeScroll,
   createElement,
   deleteProps,
@@ -32,6 +19,19 @@ import {
   setCSSProperty,
   showWarning
 } from "./chunk-HTQMM6PW.js";
+import {
+  computed,
+  h,
+  inject,
+  nextTick,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUpdated,
+  provide,
+  ref,
+  watch
+} from "./chunk-X54IR6VG.js";
 import "./chunk-EQCVQC35.js";
 
 // node_modules/.pnpm/swiper@11.1.15/node_modules/swiper/shared/swiper-core.mjs
