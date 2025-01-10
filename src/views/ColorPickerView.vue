7ff59b425f6c8b6bc9f65f83fcce9975e4fb40fb
themusecatcher <themusecatcher@163.com>
2025-01-10 16:58:03 +0800

    update

--- src/views/ColorPickerView.vue
+++ src/views/ColorPickerView.vue
@@ -7,8 +7,8 @@ const value = ref('')
     <!-- <h1>{{ $route.name }} {{ $route.meta.title }}</h1> -->
     <h2 class="mt30 mb10">基本使用</h2>
     <Space :width="240">
-      <ColorPicker v-model:value="value" />
-      <n-color-picker v-model:value="value" />
+      <ColorPicker v-model:value="value" :modes="['rgb', 'hex', 'hsl', 'hsv']" />
+      <n-color-picker v-model:value="value" :modes="['rgb', 'hex', 'hsl', 'hsv']" />
     </Space>
   </div>
 </template>
