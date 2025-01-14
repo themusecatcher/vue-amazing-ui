9d0d0cb00e92b8c3d48e3bcd70eeef21471ebcee
themusecatcher <themusecatcher@163.com>
2025-01-14 09:25:52 +0800

    feat:

--- src/views/ColorPickerView.vue
+++ src/views/ColorPickerView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue'
+import type { CheckboxOption } from 'vue-amazing-ui'
 const colorValue = ref('rgba(7, 12, 18, 0.2)')
 // setTimeout(() => {
 //   colorValue.value = ''
 // }, 2000);
+const modeOptions: CheckboxOption[] = [
+  {
+    label: 'rgb',
+    value: 'rgb'
+  },
+  {
+    label: 'hex',
+    value: 'hex'
+  },
+  {
+    label: 'hsl',
+    value: 'hsl'
+  },
+  {
+    label: 'hsv',
+    value: 'hsv'
+  }
+]
+const modes = ref(['hex'])
 watchEffect(() => {
   console.log('colorValue', colorValue.value)
 })
@@ -32,6 +52,13 @@ function handleClear() {
         @confirm="handleConfirm"
         @clear="handleClear"
       />
+    </Space>
+    <h2 class="mt30 mb10">设定模式</h2>
+    <h3 class="mb10">使用 modes 设定可选模式</h3>
+    <Space :width="240">
+      <Checkbox :options="modeOptions" v-model:value="modes" />
+      <ColorPicker :modes="modes" />
+      <n-color-picker :modes="modes" />
     </Space>
     <h2 class="mt30 mb10">可清除</h2>
     <h3 class="mb10">通过在 actions 属性中添加 clear，来显示清除按钮</h3>
