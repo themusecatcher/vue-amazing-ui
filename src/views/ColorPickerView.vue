2d16aea9050c5bd304e7df5fe5b3d27b14c8d4a9
themusecatcher <themusecatcher@163.com>
2025-01-15 17:06:04 +0800

    update

--- src/views/ColorPickerView.vue
+++ src/views/ColorPickerView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue'
 import type { CheckboxOption } from 'vue-amazing-ui'
-const colorValue = ref('#070C1233')
-// setTimeout(() => {
-//   colorValue.value = ''
-// }, 2000);
+const colorValue = ref('rgba(0, 0, 0, 1)')
+const showAlpha = ref(false)
+const showPreview = ref(true)
+const show = ref(false)
 const modeOptions: CheckboxOption[] = [
   {
     label: 'rgb',
@@ -23,7 +23,18 @@ const modeOptions: CheckboxOption[] = [
     value: 'hsv'
   }
 ]
-const modes = ref(['hex'])
+const modes = ref(['rgb'])
+const actionOptions: CheckboxOption[] = [
+  {
+    label: 'confirm',
+    value: 'confirm'
+  },
+  {
+    label: 'clear',
+    value: 'clear'
+  }
+]
+const actions = ref(['confirm', 'clear'])
 watchEffect(() => {
   console.log('colorValue', colorValue.value)
 })
@@ -46,16 +57,55 @@ function handleClear() {
         :actions="['confirm', 'clear']"
       />
     </Space>
+    <h2 class="mt30 mb10">自定义面板样式</h2>
+    <Space :width="240">
+      <ColorPicker
+        v-model:value="colorValue"
+        :modes="['rgb', 'hex', 'hsl', 'hsv']"
+        :tooltip-style="{
+          width: '280px',
+          padding: '4px',
+          borderRadius: '8px'
+        }"
+      />
+    </Space>
     <h2 class="mt30 mb10">不透明度</h2>
     <h3 class="mb10">show-alpha 控制是否可调节 alpha 通道</h3>
-    <Space :width="240">
-      <ColorPicker :show-alpha="false" :actions="['confirm', 'clear']" @confirm="handleConfirm" @clear="handleClear" />
-      <n-color-picker
-        :show-alpha="false"
-        :actions="['confirm', 'clear']"
-        @confirm="handleConfirm"
-        @clear="handleClear"
-      />
+    <Space vertical>
+      <Space align="center"> showAlpha: <Switch v-model="showAlpha"></Switch> </Space>
+      <Space :width="240">
+        <ColorPicker
+          :show-alpha="showAlpha"
+          :actions="['confirm', 'clear']"
+          @confirm="handleConfirm"
+          @clear="handleClear"
+        />
+        <n-color-picker
+          :show-alpha="showAlpha"
+          :actions="['confirm', 'clear']"
+          @confirm="handleConfirm"
+          @clear="handleClear"
+        />
+      </Space>
+    </Space>
+    <h2 class="mt30 mb10">颜色预览块</h2>
+    <h3 class="mb10">使用 showPreview 控制是否展示颜色预览块</h3>
+    <Space vertical>
+      <Space align="center"> showPreview: <Switch v-model="showPreview"></Switch> </Space>
+      <Space :width="240">
+        <ColorPicker
+          :show-preview="showPreview"
+          :actions="['confirm', 'clear']"
+          @confirm="handleConfirm"
+          @clear="handleClear"
+        />
+        <n-color-picker
+          :show-alpha="showPreview"
+          :actions="['confirm', 'clear']"
+          @confirm="handleConfirm"
+          @clear="handleClear"
+        />
+      </Space>
     </Space>
     <h2 class="mt30 mb10">尺寸</h2>
     <Space :width="240">
@@ -63,7 +113,7 @@ function handleClear() {
       <ColorPicker />
       <ColorPicker size="large" />
       <n-color-picker size="small" />
-      <n-color-picker />
+      <n-color-picker show-preview />
       <n-color-picker size="large" />
     </Space>
     <h2 class="mt30 mb10">禁用</h2>
@@ -73,37 +123,75 @@ function handleClear() {
     </Space>
     <h2 class="mt30 mb10">设定模式</h2>
     <h3 class="mb10">使用 modes 设定可选模式</h3>
-    <Space :width="240">
-      <Checkbox :options="modeOptions" v-model:value="modes" />
-      <ColorPicker :modes="modes" />
-      <n-color-picker :modes="modes" />
-    </Space>
-    <h2 class="mt30 mb10">可清除</h2>
-    <h3 class="mb10">通过在 actions 属性中添加 clear，来显示清除按钮</h3>
-    <Space :width="240">
-      <ColorPicker
-        v-model:value="colorValue"
-        :actions="['confirm', 'clear']"
-        @confirm="handleConfirm"
-        @clear="handleClear"
-      />
-      <n-color-picker
-        v-model:value="colorValue"
-        :actions="['confirm', 'clear']"
-        @confirm="handleConfirm"
-        @clear="handleClear"
-      />
+    <Space vertical>
+      <Space align="center"> modes: <Checkbox :options="modeOptions" v-model:value="modes" /> </Space>
+      <Space :width="240">
+        <ColorPicker :modes="modes" />
+        <n-color-picker :modes="modes" />
+      </Space>
     </Space>
     <h2 class="mt30 mb10">预设色板</h2>
     <Space :width="240">
       <ColorPicker
         :actions="['confirm', 'clear']"
-        :swatches="['#FFFFFF', '#18A058', '#2080F0', '#F0A020', 'rgba(208, 48, 80, 1)', '#1677ff', '#ff6900']"
+        :swatches="[
+          '#FFFFFF',
+          '#18A058',
+          '#2080F0',
+          '#F0A020',
+          '#1677ff',
+          '#ff6900',
+          'rgba(0, 0, 0, 0.88)',
+          'rgba(208, 48, 80, 1)'
+        ]"
       />
       <n-color-picker
         :actions="['confirm', 'clear']"
-        :swatches="['#FFFFFF', '#18A058', '#2080F0', '#F0A020', 'rgba(208, 48, 80, 1)', '#1677ff', '#ff6900']"
+        :swatches="[
+          '#FFFFFF',
+          '#18A058',
+          '#2080F0',
+          '#F0A020',
+          '#1677ff',
+          '#ff6900',
+          'rgba(0, 0, 0, 0.88)',
+          'rgba(208, 48, 80, 1)'
+        ]"
       />
+    </Space>
+    <h2 class="mt30 mb10">显示按钮</h2>
+    <h3 class="mb10">通过在 actions 属性中添加 confirm clear，来显示确认/清除按钮</h3>
+    <Space vertical>
+      <Space align="center"> actions: <Checkbox :options="actionOptions" v-model:value="actions" /> </Space>
+      <Space :width="240">
+        <ColorPicker v-model:value="colorValue" :actions="actions" @confirm="handleConfirm" @clear="handleClear" />
+        <n-color-picker v-model:value="colorValue" :actions="actions" @confirm="handleConfirm" @clear="handleClear" />
+      </Space>
+    </Space>
+    <h2 class="mt30 mb10">额外页脚</h2>
+    <Space :width="240">
+      <ColorPicker
+        v-model:value="colorValue"
+        :actions="['confirm', 'clear']"
+        @confirm="handleConfirm"
+        @clear="handleClear"
+      >
+        <template #footer> extra footer </template>
+      </ColorPicker>
+      <n-color-picker
+        v-model:value="colorValue"
+        :actions="['confirm', 'clear']"
+        @confirm="handleConfirm"
+        @clear="handleClear"
+      >
+        <template #action> 你好 </template>
+      </n-color-picker>
+    </Space>
+    <h2 class="mt30 mb10">使用按钮控制面板</h2>
+    <Space :width="240">
+      <ColorPicker v-model:show="show" :actions="['confirm', 'clear']" @confirm="handleConfirm" @clear="handleClear" />
+      <Button type="primary" @click="show = true">显示</Button>
+      <Button @click="show = false">隐藏</Button>
     </Space>
   </div>
 </template>
