40023cc70f405653e7e6e72500f534879f99d210
themusecatcher <themusecatcher@163.com>
2025-01-15 18:04:26 +0800

    chore: update

--- components.d.ts
+++ components.d.ts
@@ -7,12 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
-    AInput: typeof import('ant-design-vue/es')['Input']
-    AMenu: typeof import('ant-design-vue/es')['Menu']
-    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
-    NColorPicker: typeof import('naive-ui')['NColorPicker']
-    NInput: typeof import('naive-ui')['NInput']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
