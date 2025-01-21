8b3facf0905d279b6d15a0ead7e395619d10e323
themusecatcher <themusecatcher@163.com>
2025-01-21 14:11:33 +0800

    chore: update ColorPicker

--- src/views/ColorPickerView.vue
+++ src/views/ColorPickerView.vue
@@ -58,18 +58,18 @@ function handleClear() {
   <div>
     <h1>{{ $route.name }} {{ $route.meta.title }}</h1>
     <h2 class="mt30 mb10">基本使用</h2>
-    <Space :width="230">
+    <Space :width="240">
       <ColorPicker v-model:value="colorValue" />
     </Space>
     <h2 class="mt30 mb10">自定义展示内容</h2>
-    <Space :width="230">
+    <Space :width="240">
       <ColorPicker :label="labelFormat" />
       <ColorPicker>
         <template #label="{ color }"> I'm {{ color }} </template>
       </ColorPicker>
     </Space>
     <h2 class="mt30 mb10">自定义面板样式</h2>
-    <Space :width="230">
+    <Space :width="240">
       <ColorPicker
         :tooltip-style="{
           width: '280px',
@@ -82,7 +82,7 @@ function handleClear() {
     <h3 class="mb10">show-alpha 控制是否可调节 alpha 通道</h3>
     <Space vertical>
       <Space align="center"> showAlpha: <Switch v-model="showAlpha"></Switch> </Space>
-      <Space :width="230">
+      <Space :width="240">
         <ColorPicker :show-alpha="showAlpha" />
       </Space>
     </Space>
@@ -90,30 +90,30 @@ function handleClear() {
     <h3 class="mb10">使用 showPreview 控制是否展示颜色预览块；点击颜色预览块可以触发浏览器原生的颜色选择器</h3>
     <Space vertical>
       <Space align="center"> showPreview: <Switch v-model="showPreview"></Switch> </Space>
-      <Space :width="230">
+      <Space :width="240">
         <ColorPicker :show-preview="showPreview" />
       </Space>
     </Space>
     <h2 class="mt30 mb10">尺寸</h2>
-    <Space :width="230">
+    <Space :width="240">
       <ColorPicker size="small" />
       <ColorPicker />
       <ColorPicker size="large" />
     </Space>
     <h2 class="mt30 mb10">禁用</h2>
-    <Space :width="230">
+    <Space :width="240">
       <ColorPicker disabled />
     </Space>
     <h2 class="mt30 mb10">设定模式</h2>
     <h3 class="mb10">使用 modes 设定可选模式</h3>
     <Space vertical>
       <Space align="center"> modes: <Checkbox :options="modeOptions" v-model:value="modes" /> </Space>
-      <Space :width="230">
+      <Space :width="240">
         <ColorPicker :modes="modes" />
       </Space>
     </Space>
     <h2 class="mt30 mb10">预设色板</h2>
-    <Space :width="230">
+    <Space :width="240">
       <ColorPicker
         :swatches="[
           '#FFFFFF',
@@ -131,19 +131,19 @@ function handleClear() {
     <h3 class="mb10">通过在 actions 属性中添加 confirm clear，来显示确认/清除按钮</h3>
     <Space vertical>
       <Space align="center"> actions: <Checkbox :options="actionOptions" v-model:value="actions" /> </Space>
-      <Space :width="230">
+      <Space :width="240">
         <ColorPicker :actions="actions" @complete="handleComplele" @confirm="handleConfirm" @clear="handleClear" />
       </Space>
     </Space>
     <h2 class="mt30 mb10">额外页脚</h2>
-    <Space :width="230">
+    <Space :width="240">
       <ColorPicker>
         <template #footer>extra footer</template>
       </ColorPicker>
     </Space>
     <h2 class="mt30 mb10">使用按钮控制面板</h2>
     <Space>
-      <Space :width="230">
+      <Space :width="240">
         <ColorPicker v-model:show="show" />
       </Space>
       <Button type="primary" @click="show = true">显示</Button>
