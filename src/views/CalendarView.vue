4ab21d571f2ac85db66d2f9c3bd76e5dff24567a
themusecatcher <themusecatcher@163.com>
2025-01-22 17:50:52 +0800

    chore: update

---
+++ src/views/CalendarView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { h, ref } from 'vue'
+</script>
+<template>
+  <div>
+    <h1>{{ $route.name }} {{ $route.meta.title }}</h1>
+    <h2 class="mt30 mb10">基本使用</h2>
+    <Calendar />
+  </div>
+</template>
+<style lang="less" scoped>
+</style>
