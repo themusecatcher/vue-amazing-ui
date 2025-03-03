2b08f3d7e7a0e4c6658a81b1cf65c6e671c99669
themusecatcher <themusecatcher@163.com>
2025-03-03 14:09:39 +0800

    chore: update FloatButton

--- src/views/FloatButtonView.vue
+++ src/views/FloatButtonView.vue
@@ -216,7 +216,9 @@ function onOpenChange(open: boolean) {
     </Card>
     <h2 class="mt30 mb10">自定义主题色</h2>
     <Flex vertical>
-      <Space align="center">floatButtonPrimaryColor:<ColorPicker style="width: 200px" v-model:value="primaryColor" /></Space>
+      <Space align="center"
+        >floatButtonPrimaryColor:<ColorPicker style="width: 200px" v-model:value="primaryColor"
+      /></Space>
       <Space align="center"
         >floatButtonPrimaryColorHover:<ColorPicker style="width: 200px" v-model:value="primaryColorHover"
       /></Space>
