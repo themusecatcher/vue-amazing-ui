22bdd5f731337cae4596019cb5cbd10146672a6d
themusecatcher <themusecatcher@163.com>
2025-03-06 15:21:31 +0800

    chore: update

--- components/utils/resolver.ts
+++ components/utils/resolver.ts
@@ -122,24 +122,24 @@ function getSideEffects(componentName: string, options?: VueAmazingUIResolverOpt
   if (componentName === 'DatePicker') {
     // 特殊处理 DatePicker 组件样式依赖文件
     sideEffects.push(
-      `vue-amazing-ui/${type}/node_modules/.pnpm/@vuepic_vue-datepicker@11.0.1_vue@3.5.13_typescript@5.7.3_/node_modules/@vuepic/vue-datepicker/dist/main.css`
+      `vue-amazing-ui/${type}/node_modules/.pnpm/@vuepic_vue-datepicker@11.0.1_vue@3.5.13_typescript@5.8.2_/node_modules/@vuepic/vue-datepicker/dist/main.css`
     )
   }
   if (componentName === 'Swiper') {
     // 特殊处理 Swiper 组件样式依赖文件
-    sideEffects.push(`vue-amazing-ui/${type}/node_modules/.pnpm/swiper@11.2.4/node_modules/swiper/swiper.css`)
+    sideEffects.push(`vue-amazing-ui/${type}/node_modules/.pnpm/swiper@11.2.5/node_modules/swiper/swiper.css`)
     const swiperModulesStyle = [
-      'navigation',
-      'pagination',
       'effect-cards',
       'effect-creative',
       'effect-cube',
       'effect-fade',
-      'effect-flip'
+      'effect-flip',
+      'navigation',
+      'pagination'
     ]
     swiperModulesStyle.forEach((moduleName) => {
       sideEffects.push(
-        `vue-amazing-ui/${type}/node_modules/.pnpm/swiper@11.2.4/node_modules/swiper/modules/${moduleName}.css`
+        `vue-amazing-ui/${type}/node_modules/.pnpm/swiper@11.2.5/node_modules/swiper/modules/${moduleName}.css`
       )
     })
   }
