3284d2e2175e4a1ac1fe7990ad4e2607da0b87f2
themusecatcher <themusecatcher@163.com>
2025-05-02 21:17:34 +0800

    update Vue Amazing UI

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import 'less/global.less'
 
-// import VueAmazingUI from 'components/index'
-import VueAmazingUI from 'vue-amazing-ui'
-import 'vue-amazing-ui/css'
+import VueAmazingUI from 'components/index'
+// import VueAmazingUI from 'vue-amazing-ui'
+// import 'vue-amazing-ui/css'
 
 const app = createApp(App)
 // window.rafTimeout = rafTimeout // 挂载到window上，全局可用，无需引入
