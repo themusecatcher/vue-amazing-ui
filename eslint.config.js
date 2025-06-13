d6645c7539a2e7b1c5e477294bd1b630ddaeeed2
themusecatcher <themusecatcher@163.com>
2025-06-13 14:41:02 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -10,15 +10,23 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{ts,mts,tsx,vue}']
   },
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
   },
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+  // 添加自定义规则
+  {
+    name: 'custom-eslint-rules',
+    rules: {
+      // 示例：关闭 @typescript-eslint/no-explicit-any 规则
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
 )
