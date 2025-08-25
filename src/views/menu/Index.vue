283f1e6f97ff58d4a849dee2e561cb91bc98d1f6
themusecatcher <themusecatcher@163.com>
2025-08-25 10:38:43 +0800

    feat: add layout & menu

---
+++ src/views/menu/Index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref, h } from 'vue'
+</script>
+<template>
+  <div>
+    <h1>{{ $route.name }} {{ $route.meta.title }}</h1>
+    <h2 class="mt30 mb10">基本使用</h2>
+  </div>
+</template>
+<style lang="less" scoped></style>
